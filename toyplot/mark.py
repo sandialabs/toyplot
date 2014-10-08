de09c51c3c976f08d10e04908e6e12b7ee8c0ba0
Timothy M. Shead <tshead@sandia.gov>
2014-10-08 00:17:22 -0600

    Organizing code.

--- toyplot/mark.py
+++ toyplot/mark.py
@@ -9,228 +9,15 @@ import numpy
 import toyplot.require
 
 
-class Mark(object):
-  def __init__(self, *styles, **kwargs):
-    self._style = toyplot.style.combine(*styles)
-    self._id = kwargs.get("id", None)
-
 ###############################################################################################
 # Basic Toyplot marks
 
-class Legend(Mark):
-  """Render a figure legend (a collection of markers and labels).
-
-  Do not create Legend instances directly.  Use factory methods such as
-  :meth:`toyplot.canvas.Canvas.legend` or :meth:`toyplot.axes.Cartesian.legend` instead.
+class Mark(object):
+  """Base class for all Toyplot marks.
   """
-  def __init__(self, xmin, xmax, ymin, ymax, marks, style, label_style, id):
-    Mark.__init__(self, {"fill":"none", "stroke":"none"}, style, id=id)
-    self._xmin = xmin
-    self._xmax = xmax
-    self._ymin = ymin
-    self._ymax = ymax
-    self._gutter = 10
-    self._marks = marks
-    self._label_style = label_style
-
-class VColorBar(Mark):
-  """Displays a one-dimensional mapping from values to colors.
-
-  Do not create VColorbarMark instances directly.  Use factory methods such
-  as :meth:`toyplot.axes.Cartesian.colorbar` instead.
-  """
-  class DomainHelper(object):
-    def __init__(self, min, max):
-      self._min = min
-      self._max = max
-    @property
-    def min(self):
-      return self._min
-    @min.setter
-    def min(self, value):
-      self._min = value
-    @property
-    def max(self):
-      return self._max
-    @max.setter
-    def max(self, value):
-      self._max = value
-
-  class LabelHelper(object):
-    def __init__(self, label, style):
-      self._text = label
-      self._style = toyplot.style.combine({"font-size":"12px", "font-weight":"bold", "stroke":"none", "text-anchor":"middle", "alignment-baseline":"middle", "baseline-shift":"-200%"}, toyplot.require.style(style))
-    @property
-    def text(self):
-      return self._text
-    @text.setter
-    def text(self, value):
-      self._text = value
-    @property
-    def style(self):
-      return self._style
-    @style.setter
-    def style(self, value):
-      self._style = toyplot.style.combine(self._style, toyplot.require.style(value))
-
-  class PerTickHelper(object):
-    class TickProxy(object):
-      def __init__(self, tick):
-        self._tick = tick
-      @property
-      def style(self):
-        return self._tick.get("style", {})
-      @style.setter
-      def style(self, value):
-        self._tick["style"] = toyplot.style.combine(self._tick.get("style"), toyplot.require.style(value))
-    def __init__(self):
-      self._indices = collections.defaultdict(dict)
-      self._values = collections.defaultdict(dict)
-    def __call__(self, index=None, value=None, style=None):
-      if index is None and value is None:
-        raise ValueError("Must specify tick index or value.")
-      if index is not None and value is not None:
-        raise ValueError("Must specify either index or value, not both.")
-      if index is not None:
-        return VColorBar.PerTickHelper.TickProxy(self._indices[index])
-      if value is not None:
-        return VColorBar.PerTickHelper.TickProxy(self._values[value])
-    def styles(self, values):
-      results = [self._indices[index].get("style", None) if index in self._indices else None for index in range(len(values))]
-      for value in self._values:
-        deltas = numpy.abs(values - value)
-        results[numpy.argmin(deltas)] = self._values[value].get("style", None)
-      return results
-
-  class TicksHelper(object):
-    def __init__(self, length, locator):
-      self._locator = locator
-      self._show = False
-      self._length = length
-      self._style = {}
-      self.labels = VColorBar.TickLabelsHelper()
-      self.tick = VColorBar.PerTickHelper()
-    @property
-    def locator(self):
-      return self._locator
-    @locator.setter
-    def locator(self, value):
-      self._locator = value
-    @property
-    def show(self):
-      return self._show
-    @show.setter
-    def show(self, value):
-      self._show = value
-    @property
-    def length(self):
-      return self._length
-    @length.setter
-    def length(self, value):
-      self._length = value
-    @property
-    def style(self):
-      return self._style
-    @style.setter
-    def style(self, value):
-      self._style = toyplot.style.combine(self._style, toyplot.require.style(value))
-
-  class TickLabelsHelper(object):
-    def __init__(self):
-      self._show = True
-      self._style = {"font-size":"10px", "font-weight":"normal", "stroke":"none"}
-      self.label = VColorBar.PerTickHelper()
-    @property
-    def show(self):
-      return self._show
-    @show.setter
-    def show(self, value):
-      self._show = value
-    @property
-    def style(self):
-      return self._style
-    @style.setter
-    def style(self, value):
-      self._style = toyplot.style.combine(self._style, toyplot.require.style(value))
-
-  def __init__(self, xmin_range, xmax_range, ymin_range, ymax_range, label, colormap, padding, tick_length, min, max, tick_locator, style, id):
-    Mark.__init__(self, style)
-
-    self._xmin_range = xmin_range
-    self._xmax_range = xmax_range
-    self._ymin_range = ymin_range
-    self._ymax_range = ymax_range
-    self._scale = "linear"
-    self._colormap = colormap
-    self._vmin_implicit = None
-    self._vmax_implicit = None
-    self._padding = padding
-    self.domain = VColorBar.DomainHelper(min, max)
-    self.label = VColorBar.LabelHelper(label=label, style=None)
-    self.ticks = VColorBar.TicksHelper(tick_length, tick_locator)
-    self._id = id
-
-  def _update_domain(self, vmin, vmax):
-    self._vmin_implicit = vmin if self._vmin_implicit is None else self._vmin_implicit if vmin is None else min(vmin, self._vmin_implicit)
-    self._vmax_implicit = vmax if self._vmax_implicit is None else self._vmax_implicit if vmin is None else max(vmax, self._vmax_implicit)
-
-  def _finalize_domain(self):
-    # Begin with the implicit domain defined by any explicitly-specified data.
-    vmin = self._vmin_implicit
-    vmax = self._vmax_implicit
-
-    # If there is no implicit domain (we don't have any data), default to the origin.
-    if vmin is None:
-      vmin = 0
-    if vmax is None:
-      vmax = 0
-
-    # Allow users to override the domain.
-    if self.domain._min is not None:
-      vmin = self.domain._min
-    if self.domain._max is not None:
-      vmax = self.domain._max
-
-    # Ensure that the domain is never empty.
-    if vmin == vmax:
-      vmin -= 0.5
-      vmax += 0.5
-
-    def get_locator(locator, scale, domain_min, domain_max):
-      if locator is not None:
-        return locator
-      if scale == "linear":
-        return toyplot.locator.Extended()
-
-    # Calculate tick locations and labels.
-    locator = get_locator(self.ticks._locator, self._scale, vmin, vmax)
-
-    self._tick_locations, self._tick_labels, self._tick_titles = locator.ticks(vmin, vmax)
-
-    # Allow tick locations to grow (never shrink) the domain.
-    if len(self._tick_locations):
-      vmin = min(vmin, self._tick_locations[0])
-      vmax = max(vmax, self._tick_locations[-1])
-
-    self._vmin_computed = vmin
-    self._vmax_computed = vmax
-
-    def linear_projection(domain_min, domain_max, range_min, range_max):
-      def implementation(x):
-        return (1 - ((x - domain_min) / (domain_max - domain_min))) * (range_max - range_min) + range_min
-      return implementation
-
-    if self._scale == "linear":
-      self._projection = linear_projection(vmin, vmax, self._ymin_range + self._padding, self._ymax_range - self._padding)
-
-  def _project_y(self, v):
-    return self._projection(v)
-
-  def _project_color(self, v):
-    return self._colormap.colors(v, self._vmin_computed, self._vmax_computed)
-
-###############################################################################################
-# Higher-level marks
+  def __init__(self, *styles, **kwargs):
+    self._style = toyplot.style.combine(*styles)
+    self._id = kwargs.get("id", None)
 
 class AxisLines(Mark):
   """Render multiple lines parallel to an axis.
@@ -445,4 +232,219 @@ class Text(Mark):
     self._fill = fill               # 1 fill color column
     self._opacity = opacity         # 1 opacity column
     self._title = title             # 1 title column
+
+###############################################################################################
+# More specialized marks
+
+class Legend(Mark):
+  """Render a figure legend (a collection of markers and labels).
+
+  Do not create Legend instances directly.  Use factory methods such as
+  :meth:`toyplot.canvas.Canvas.legend` or :meth:`toyplot.axes.Cartesian.legend` instead.
+  """
+  def __init__(self, xmin, xmax, ymin, ymax, marks, style, label_style, id):
+    Mark.__init__(self, {"fill":"none", "stroke":"none"}, style, id=id)
+    self._xmin = xmin
+    self._xmax = xmax
+    self._ymin = ymin
+    self._ymax = ymax
+    self._gutter = 10
+    self._marks = marks
+    self._label_style = label_style
+
+class VColorBar(Mark):
+  """Displays a one-dimensional mapping from values to colors.
+
+  Do not create VColorbarMark instances directly.  Use factory methods such
+  as :meth:`toyplot.axes.Cartesian.colorbar` instead.
+  """
+  class DomainHelper(object):
+    def __init__(self, min, max):
+      self._min = min
+      self._max = max
+    @property
+    def min(self):
+      return self._min
+    @min.setter
+    def min(self, value):
+      self._min = value
+    @property
+    def max(self):
+      return self._max
+    @max.setter
+    def max(self, value):
+      self._max = value
+
+  class LabelHelper(object):
+    def __init__(self, label, style):
+      self._text = label
+      self._style = toyplot.style.combine({"font-size":"12px", "font-weight":"bold", "stroke":"none", "text-anchor":"middle", "alignment-baseline":"middle", "baseline-shift":"-200%"}, toyplot.require.style(style))
+    @property
+    def text(self):
+      return self._text
+    @text.setter
+    def text(self, value):
+      self._text = value
+    @property
+    def style(self):
+      return self._style
+    @style.setter
+    def style(self, value):
+      self._style = toyplot.style.combine(self._style, toyplot.require.style(value))
+
+  class PerTickHelper(object):
+    class TickProxy(object):
+      def __init__(self, tick):
+        self._tick = tick
+      @property
+      def style(self):
+        return self._tick.get("style", {})
+      @style.setter
+      def style(self, value):
+        self._tick["style"] = toyplot.style.combine(self._tick.get("style"), toyplot.require.style(value))
+    def __init__(self):
+      self._indices = collections.defaultdict(dict)
+      self._values = collections.defaultdict(dict)
+    def __call__(self, index=None, value=None, style=None):
+      if index is None and value is None:
+        raise ValueError("Must specify tick index or value.")
+      if index is not None and value is not None:
+        raise ValueError("Must specify either index or value, not both.")
+      if index is not None:
+        return VColorBar.PerTickHelper.TickProxy(self._indices[index])
+      if value is not None:
+        return VColorBar.PerTickHelper.TickProxy(self._values[value])
+    def styles(self, values):
+      results = [self._indices[index].get("style", None) if index in self._indices else None for index in range(len(values))]
+      for value in self._values:
+        deltas = numpy.abs(values - value)
+        results[numpy.argmin(deltas)] = self._values[value].get("style", None)
+      return results
+
+  class TicksHelper(object):
+    def __init__(self, length, locator):
+      self._locator = locator
+      self._show = False
+      self._length = length
+      self._style = {}
+      self.labels = VColorBar.TickLabelsHelper()
+      self.tick = VColorBar.PerTickHelper()
+    @property
+    def locator(self):
+      return self._locator
+    @locator.setter
+    def locator(self, value):
+      self._locator = value
+    @property
+    def show(self):
+      return self._show
+    @show.setter
+    def show(self, value):
+      self._show = value
+    @property
+    def length(self):
+      return self._length
+    @length.setter
+    def length(self, value):
+      self._length = value
+    @property
+    def style(self):
+      return self._style
+    @style.setter
+    def style(self, value):
+      self._style = toyplot.style.combine(self._style, toyplot.require.style(value))
+
+  class TickLabelsHelper(object):
+    def __init__(self):
+      self._show = True
+      self._style = {"font-size":"10px", "font-weight":"normal", "stroke":"none"}
+      self.label = VColorBar.PerTickHelper()
+    @property
+    def show(self):
+      return self._show
+    @show.setter
+    def show(self, value):
+      self._show = value
+    @property
+    def style(self):
+      return self._style
+    @style.setter
+    def style(self, value):
+      self._style = toyplot.style.combine(self._style, toyplot.require.style(value))
+
+  def __init__(self, xmin_range, xmax_range, ymin_range, ymax_range, label, colormap, padding, tick_length, min, max, tick_locator, style, id):
+    Mark.__init__(self, style)
+
+    self._xmin_range = xmin_range
+    self._xmax_range = xmax_range
+    self._ymin_range = ymin_range
+    self._ymax_range = ymax_range
+    self._scale = "linear"
+    self._colormap = colormap
+    self._vmin_implicit = None
+    self._vmax_implicit = None
+    self._padding = padding
+    self.domain = VColorBar.DomainHelper(min, max)
+    self.label = VColorBar.LabelHelper(label=label, style=None)
+    self.ticks = VColorBar.TicksHelper(tick_length, tick_locator)
+    self._id = id
+
+  def _update_domain(self, vmin, vmax):
+    self._vmin_implicit = vmin if self._vmin_implicit is None else self._vmin_implicit if vmin is None else min(vmin, self._vmin_implicit)
+    self._vmax_implicit = vmax if self._vmax_implicit is None else self._vmax_implicit if vmin is None else max(vmax, self._vmax_implicit)
+
+  def _finalize_domain(self):
+    # Begin with the implicit domain defined by any explicitly-specified data.
+    vmin = self._vmin_implicit
+    vmax = self._vmax_implicit
+
+    # If there is no implicit domain (we don't have any data), default to the origin.
+    if vmin is None:
+      vmin = 0
+    if vmax is None:
+      vmax = 0
+
+    # Allow users to override the domain.
+    if self.domain._min is not None:
+      vmin = self.domain._min
+    if self.domain._max is not None:
+      vmax = self.domain._max
+
+    # Ensure that the domain is never empty.
+    if vmin == vmax:
+      vmin -= 0.5
+      vmax += 0.5
+
+    def get_locator(locator, scale, domain_min, domain_max):
+      if locator is not None:
+        return locator
+      if scale == "linear":
+        return toyplot.locator.Extended()
+
+    # Calculate tick locations and labels.
+    locator = get_locator(self.ticks._locator, self._scale, vmin, vmax)
+
+    self._tick_locations, self._tick_labels, self._tick_titles = locator.ticks(vmin, vmax)
+
+    # Allow tick locations to grow (never shrink) the domain.
+    if len(self._tick_locations):
+      vmin = min(vmin, self._tick_locations[0])
+      vmax = max(vmax, self._tick_locations[-1])
+
+    self._vmin_computed = vmin
+    self._vmax_computed = vmax
+
+    def linear_projection(domain_min, domain_max, range_min, range_max):
+      def implementation(x):
+        return (1 - ((x - domain_min) / (domain_max - domain_min))) * (range_max - range_min) + range_min
+      return implementation
+
+    if self._scale == "linear":
+      self._projection = linear_projection(vmin, vmax, self._ymin_range + self._padding, self._ymax_range - self._padding)
+
+  def _project_y(self, v):
+    return self._projection(v)
+
+  def _project_color(self, v):
+    return self._colormap.colors(v, self._vmin_computed, self._vmax_computed)
 

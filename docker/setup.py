a0e30b248570ba525a3e6279c3c7f3ec0a443a6e
Timothy M. Shead <tshead@sandia.gov>
2014-10-26 22:45:04 -0600

    Checkpoint work on a continuous integration container.

---
+++ docker/setup.py
@@ -0,0 +1,17 @@
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("command", nargs="?", default="build", choices=["build", "push"])
+arguments = parser.parse_args()
+
+images = ["supervisord", "sshd", "buildbot"]
+
+if arguments.command == "build":
+  for image in images:
+    subprocess.check_call(["docker", "build", "-t", "toyplot/%s" % image, image])
+
+if arguments.command == "push":
+  for image in images:
+    subprocess.check_call(["docker", "push", "toyplot/%s" % image])
+

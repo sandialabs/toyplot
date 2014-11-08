f463247198354b0af1d0b8a4ff63c0757d4c2839
Timothy M. Shead <tshead@sandia.gov>
2014-11-07 17:35:30 -0700

    Exclude the testing module from coverage results.

--- regression.py
+++ regression.py
@@ -1,6 +1,6 @@
 import subprocess
 
-subprocess.check_call(["coverage", "run", "--source", "toyplot", "-m", "nose"])
-subprocess.check_call(["coverage", "run", "--append", "--source", "toyplot", "-m", "behave"])
+subprocess.check_call(["coverage", "run", "--source", "toyplot", "--omit", "toyplot/testing.py", "-m", "nose"])
+subprocess.check_call(["coverage", "run", "--append", "--source", "toyplot", "--omit", "toyplot/testing.py", "-m", "behave"])
 subprocess.check_call(["coverage", "report"])
 subprocess.check_call(["coverage", "html", "--directory", ".cover"])

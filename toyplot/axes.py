0d7d7db925366fb3e794ca3387ce16a49472c521
Timothy M. Shead <tshead@sandia.gov>
2014-12-15 20:52:04 -0700

    Ensure that visible cells are rendered in a deterministic order.

--- toyplot/axes.py
+++ toyplot/axes.py
@@ -56,6 +56,65 @@ def _symmetric_log(x, base, threshold=1):
     return x
   return numpy.sign(x) * (threshold + numpy.log10(numpy.abs(x)) / numpy.log10(base))
 
+class OrderedSet(collections.MutableSet):
+  """Python recipe from http://code.activestate.com/recipes/576694-orderedset
+  """
+  def __init__(self, iterable=None):
+    self.end = end = []
+    end += [None, end, end]         # sentinel node for doubly linked list
+    self.map = {}                   # key --> [key, prev, next]
+    if iterable is not None:
+      self |= iterable
+
+  def __len__(self):
+    return len(self.map)
+
+  def __contains__(self, key):
+    return key in self.map
+
+  def add(self, key):
+    if key not in self.map:
+      end = self.end
+      curr = end[1]
+      curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+  def discard(self, key):
+    if key in self.map:
+      key, prev, next = self.map.pop(key)
+      prev[2] = next
+      next[1] = prev
+
+  def __iter__(self):
+    end = self.end
+    curr = end[2]
+    while curr is not end:
+      yield curr[0]
+      curr = curr[2]
+
+  def __reversed__(self):
+    end = self.end
+    curr = end[1]
+    while curr is not end:
+      yield curr[0]
+      curr = curr[1]
+
+  def pop(self, last=True):
+    if not self:
+      raise KeyError('set is empty')
+    key = self.end[1][0] if last else self.end[2][0]
+    self.discard(key)
+    return key
+
+  def __repr__(self):
+    if not self:
+      return '%s()' % (self.__class__.__name__,)
+    return '%s(%r)' % (self.__class__.__name__, list(self))
+
+  def __eq__(self, other):
+    if isinstance(other, OrderedSet):
+      return len(self) == len(other) and list(self) == list(other)
+    return set(self) == set(other)
+
 ###############################################################################################
 # Cartesian
 
@@ -1378,7 +1437,7 @@ class Table(object):
       for row in range(rows):
         for column in range(columns):
           self._cells[row,column] = Table.Cell(row, column, align, style)
-      self._visible_cells = set(numpy.ravel(self._cells))
+      self._visible_cells = OrderedSet(numpy.ravel(self._cells))
 
     def _column_widths(self):
       column_widths = numpy.zeros(self._cells.shape[1])

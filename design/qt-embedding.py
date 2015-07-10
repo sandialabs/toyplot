aa47f0721538e3b4d3242e89c2acd0781966fe1e
Timothy M. Shead <tshead@sandia.gov>
2015-07-10 10:19:07 -0600

    Use PyQt5 for the qt-embedding example, which bypasses bugs in older versions of QWebView.

--- design/qt-embedding.py
+++ design/qt-embedding.py
@@ -1,5 +1,13 @@
-from PySide.QtGui import *
-from PySide.QtWebKit import *
+# Note: we prefer PyQt5 only because we've had issues embedding our HTML output
+# with specific versions (Qt 4.8.7 on a Mac) of QWebView.  Otherwise, the
+# functionality is equivalent.
+
+try:
+  from PyQt5.QtWidgets import *
+  from PyQt5.QtWebKitWidgets import *
+except:
+  from PySide.QtGui import *
+  from PySide.QtWebKit import *
 
 import numpy
 import sys
@@ -10,13 +18,19 @@ application = QApplication(sys.argv)
 window = QWebView()
 
 canvas, axes, mark = toyplot.plot(numpy.linspace(0, 1) ** 2)
-# Embed SVG in the QWebView - this works.
-window.setContent(xml.tostring(toyplot.svg.render(canvas)), "image/svg+xml")
-# Embed PNG in the QWebView - this works.
-#window.setContent(toyplot.png.render(canvas), "image/png")
-# Embed HTML in the QWebView - for unknown reasons, this doesn't work for me.
-#window.setHtml(xml.tostring(toyplot.html.render(canvas), method="html"))
 
-window.settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
+# Embed Toyplot HTML output in a QWebView.  This is the best option because it
+# retains figure interaction.
+window.setHtml(xml.tostring(toyplot.html.render(canvas), method="html"))
+
+# Embed Toyplot SVG in a QWebView.  This retains the figure quality, but no
+# interaction.
+#window.setContent(xml.tostring(toyplot.svg.render(canvas)), "image/svg+xml")
+
+# Embed Toyplot PNG in a QWebView.  This requires additional Python
+# dependencies, doesn't scale cleanly, and has no interaction.
+#window.setContent(toyplot.png.render(canvas), "image/png")
+
+#window.settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
 window.show()
 application.exec_()

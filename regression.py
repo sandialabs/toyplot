999a0101772e10366b4a31c22398adefd26b7e17
Timothy M. Shead <tshead@sandia.gov>
2015-08-10 20:24:09 -0600

    Limit the files searched by nose for tests.

--- regression.py
+++ regression.py
@@ -1,7 +1,7 @@
 import subprocess
 
 subprocess.call(["coverage", "run", "--source", "toyplot",
-                 "--omit", "toyplot/testing.py", "-m", "nose"])
+                 "--omit", "toyplot/testing.py", "-m", "nose", "--exclude-dir", "toyplot"])
 subprocess.call(["coverage", "run", "--append", "--source",
                  "toyplot", "--omit", "toyplot/testing.py", "-m", "behave"])
 subprocess.call(["coverage", "report"])

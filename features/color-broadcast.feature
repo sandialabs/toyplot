b0a1836803532e8583bd64988ad4da5a2f0a44d8
Timothy M. Shead <tshead@sandia.gov>
2015-09-05 13:31:00 -0600

    Test color broadcasting use-cases for fills, and cleanup the testing code.

--- features/color-broadcast.feature
+++ features/color-broadcast.feature
@@ -40,6 +40,11 @@ Feature: Color broadcasting
 
     Scenario Outline: Color broadcasting use-cases
         Given a set of cartesian axes
+        And a set of diverging series
+        And a set of per-series values
+        And a set of per-series colors
+        And a set of per-datum values
+        And a set of per-datum colors
         Then <mark> can be rendered with <color>
 
         Examples:
@@ -50,18 +55,33 @@ Feature: Color broadcasting
             | bars                         | per-datum explicit colors                            |
             | bars                         | palette colors                                       |
             | bars                         | colormap colors                                      |
-            | bars                         | value colors                                         |
-            | bars                         | value + palette colors                               |
-            | bars                         | value + colormap colors                              |
-            | hlines                       | default colors                                       |
-            | hlines                       | palette colors                                       |
-            | hlines                       | colormap colors                                      |
-            | hlines                       | value colors                                         |
-            | hlines                       | value + palette colors                               |
-            | hlines                       | value + colormap colors                              |
-            | vlines                       | default colors                                       |
-            | vlines                       | palette colors                                       |
-            | vlines                       | colormap colors                                      |
-            | vlines                       | value colors                                         |
-            | vlines                       | value + palette colors                               |
-            | vlines                       | value + colormap colors                              |
+            | bars                         | per-series value colors                              |
+            | bars                         | per-series value + palette colors                    |
+            | bars                         | per-series value + colormap colors                   |
+            | bars                         | per-datum value colors                               |
+            | bars                         | per-datum value + palette colors                     |
+            | bars                         | per-datum value + colormap colors                    |
+            | fills                         | default colors                                       |
+            | fills                         | one explicit color                                   |
+            | fills                         | per-series explicit colors                           |
+            | fills                         | palette colors                                       |
+            | fills                         | colormap colors                                      |
+            | fills                         | per-series value colors                              |
+            | fills                         | per-series value + palette colors                    |
+            | fills                         | per-series value + colormap colors                   |
+            | hlines                         | default colors                                       |
+            | hlines                         | one explicit color                                   |
+            | hlines                         | per-datum explicit colors                            |
+            | hlines                         | palette colors                                       |
+            | hlines                         | colormap colors                                      |
+            | hlines                         | per-datum value colors                               |
+            | hlines                         | per-datum value + palette colors                     |
+            | hlines                         | per-datum value + colormap colors                    |
+            | vlines                         | default colors                                       |
+            | vlines                         | one explicit color                                   |
+            | vlines                         | per-datum explicit colors                            |
+            | vlines                         | palette colors                                       |
+            | vlines                         | colormap colors                                      |
+            | vlines                         | per-datum value colors                               |
+            | vlines                         | per-datum value + palette colors                     |
+            | vlines                         | per-datum value + colormap colors                    |
--- features/steps/color.py
+++ features/steps/color.py
@@ -380,175 +380,261 @@ def step_impl(context):
     nose.tools.assert_equal(colormap.css(0), "rgba(100%,0%,0%,1)")
     nose.tools.assert_equal(colormap.css(-1), "rgba(100%,100%,100%,1)")
 
+@given(u'a set of diverging series')
+def step_impl(context):
+    context.x = numpy.linspace(0, 1)
+    context.series = numpy.column_stack((context.x**2, context.x**2/2, context.x**2/3, context.x**2/4))
+
+@given(u'a set of per-series values')
+def step_impl(context):
+    context.series_values = numpy.array([1, 4, 3, 2])
+
+@given(u'a set of per-series colors')
+def step_impl(context):
+    context.series_colors = ["red", "green", "blue", "yellow"]
+
+@given(u'a set of per-datum values')
+def step_impl(context):
+    numpy.random.seed(1234)
+    context.datum_values = numpy.column_stack((
+        numpy.random.normal(loc=0, size=context.series.shape[0]),
+        numpy.random.normal(loc=3, size=context.series.shape[0]),
+        numpy.random.normal(loc=6, size=context.series.shape[0]),
+        numpy.random.normal(loc=9, size=context.series.shape[0]),
+        ))
+
+@given(u'a set of per-datum colors')
+def step_impl(context):
+    context.datum_colors = numpy.tile(toyplot.color.css("black"), context.series.shape)
+    context.datum_colors[15:20, 0] = toyplot.color.css("red")
+    context.datum_colors[25:30, 1] = toyplot.color.css("green")
+    context.datum_colors[35:40, 2] = toyplot.color.css("blue")
+    context.datum_colors[45:50, 3] = toyplot.color.css("yellow")
+
 @then(u'bars can be rendered with default colors')
 def step_impl(context):
-    m1 = numpy.linspace(0, 1)
-    m2 = numpy.linspace(0, 1) ** 2
-    series = numpy.column_stack((m1, m2))
-    context.axes.bars(series)
+    context.axes.bars(context.series, baseline="symmetric")
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-bars-default-colors")
+        context.canvas, "color-broadcast-bars-default")
 
 @then(u'bars can be rendered with one explicit color')
 def step_impl(context):
-    m1 = numpy.linspace(0, 1)
-    m2 = numpy.linspace(0, 1) ** 2
-    series = numpy.column_stack((m1, m2))
-    context.axes.bars(series, color="red")
+    context.axes.bars(context.series, baseline="symmetric", color="red")
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-bars-one-color")
+        context.canvas, "color-broadcast-bars-one-color")
 
 @then(u'bars can be rendered with per-series explicit colors')
 def step_impl(context):
-    m1 = numpy.linspace(0, 1)
-    m2 = numpy.linspace(0, 1) ** 2
-    series = numpy.column_stack((m1, m2))
-    context.axes.bars(series, color=["red", "yellow"])
+    context.axes.bars(context.series, baseline="symmetric", color=context.series_colors)
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-bars-per-series-colors")
+        context.canvas, "color-broadcast-bars-per-series-colors")
 
 @then(u'bars can be rendered with per-datum explicit colors')
 def step_impl(context):
-    m1 = numpy.linspace(0, 1)
-    m2 = numpy.linspace(0, 1) ** 2
-    series = numpy.column_stack((m1, m2))
-    color = numpy.tile(toyplot.color.css("black"), series.shape)
-    color[5:10, 0] = toyplot.color.css("red")
-    color[20:25, 1] = toyplot.color.css("blue")
-    context.axes.bars(series, color=color)
+    context.axes.bars(context.series, baseline="symmetric", color=context.datum_colors)
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-bars-per-datum-colors")
+        context.canvas, "color-broadcast-bars-per-datum-colors")
 
 @then(u'bars can be rendered with palette colors')
 def step_impl(context):
-    m1 = numpy.linspace(0, 1)
-    m2 = numpy.linspace(0, 1) ** 2
-    series = numpy.column_stack((m1, m2))
-    context.axes.bars(series, color=toyplot.color.brewer("Set1"))
+    context.axes.bars(context.series, baseline="symmetric", color=toyplot.color.brewer("Set1"))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-bars-palette-colors")
+        context.canvas, "color-broadcast-bars-palette")
 
 @then(u'bars can be rendered with colormap colors')
 def step_impl(context):
-    m1 = numpy.linspace(0, 1)
-    m2 = numpy.linspace(0, 1) ** 2
-    series = numpy.column_stack((m1, m2))
-    context.axes.bars(series, color=toyplot.color.LinearMap(toyplot.color.brewer("Set1")))
+    context.axes.bars(context.series, baseline="symmetric", color=toyplot.color.LinearMap(toyplot.color.brewer("Set1")))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-bars-colormap-colors")
+        context.canvas, "color-broadcast-bars-colormap")
 
-@then(u'bars can be rendered with value colors')
+@then(u'bars can be rendered with per-series value colors')
 def step_impl(context):
-    m1 = numpy.linspace(0, 1)
-    m2 = numpy.linspace(0, 1) ** 2
-    series = numpy.column_stack((m1, m2))
-    values = numpy.arange(numpy.product(series.shape)).reshape(series.shape, order="F")
-    context.axes.bars(series, color=values)
+    context.axes.bars(context.series, baseline="symmetric", color=context.series_values)
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-bars-values-colors")
+        context.canvas, "color-broadcast-bars-per-series-values")
 
-@then(u'bars can be rendered with value + palette colors')
+@then(u'bars can be rendered with per-series value + palette colors')
 def step_impl(context):
-    m1 = numpy.linspace(0, 1)
-    m2 = numpy.linspace(0, 1) ** 2
-    series = numpy.column_stack((m1, m2))
-    values = numpy.arange(numpy.product(series.shape)).reshape(series.shape, order="F")
-    context.axes.bars(series, color=(values, toyplot.color.brewer("Reds")))
+    context.axes.bars(context.series, baseline="symmetric", color=(context.series_values, toyplot.color.brewer("Reds")))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-bars-values-palette-colors")
+        context.canvas, "color-broadcast-bars-per-series-values-palette")
 
-@then(u'bars can be rendered with value + colormap colors')
+@then(u'bars can be rendered with per-series value + colormap colors')
 def step_impl(context):
-    m1 = numpy.linspace(0, 1)
-    m2 = numpy.linspace(0, 1) ** 2
-    series = numpy.column_stack((m1, m2))
-    values = numpy.arange(numpy.product(series.shape)).reshape(series.shape, order="F")
-    context.axes.bars(series, color=(values, toyplot.color.LinearMap()))
+    context.axes.bars(context.series, baseline="symmetric", color=(context.series_values, toyplot.color.LinearMap()))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-bars-values-colormap-colors")
+        context.canvas, "color-broadcast-bars-per-series-values-colormap")
+
+@then(u'bars can be rendered with per-datum value colors')
+def step_impl(context):
+    context.axes.bars(context.series, baseline="symmetric", color=context.datum_values)
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-bars-per-datum-values")
+
+@then(u'bars can be rendered with per-datum value + palette colors')
+def step_impl(context):
+    context.axes.bars(context.series, baseline="symmetric", color=(context.datum_values, toyplot.color.brewer("Reds")))
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-bars-per-datum-values-palette")
+
+@then(u'bars can be rendered with per-datum value + colormap colors')
+def step_impl(context):
+    context.axes.bars(context.series, baseline="symmetric", color=(context.datum_values, toyplot.color.LinearMap()))
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-bars-per-datum-values-colormap")
+
+
+@then(u'fills can be rendered with default colors')
+def step_impl(context):
+    context.axes.fill(context.series, baseline="symmetric")
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-fills-default")
+
+@then(u'fills can be rendered with one explicit color')
+def step_impl(context):
+    context.axes.fill(context.series, baseline="symmetric", color="red")
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-fills-one-color")
+
+@then(u'fills can be rendered with per-series explicit colors')
+def step_impl(context):
+    context.axes.fill(context.series, baseline="symmetric", color=context.series_colors)
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-fills-per-series-colors")
+
+@then(u'fills can be rendered with palette colors')
+def step_impl(context):
+    context.axes.fill(context.series, baseline="symmetric", color=toyplot.color.brewer("Set1"))
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-fills-palette")
+
+@then(u'fills can be rendered with colormap colors')
+def step_impl(context):
+    context.axes.fill(context.series, baseline="symmetric", color=toyplot.color.LinearMap(toyplot.color.brewer("Set1")))
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-fills-colormap")
+
+@then(u'fills can be rendered with per-series value colors')
+def step_impl(context):
+    context.axes.fill(context.series, baseline="symmetric", color=context.series_values)
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-fills-per-series-values")
+
+@then(u'fills can be rendered with per-series value + palette colors')
+def step_impl(context):
+    context.axes.fill(context.series, baseline="symmetric", color=(context.series_values, toyplot.color.brewer("Reds")))
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-fills-per-series-values-palette")
+
+@then(u'fills can be rendered with per-series value + colormap colors')
+def step_impl(context):
+    context.axes.fill(context.series, baseline="symmetric", color=(context.series_values, toyplot.color.LinearMap()))
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-fills-per-series-values-colormap")
+
+
 
 @then(u'hlines can be rendered with default colors')
 def step_impl(context):
-    x = numpy.linspace(0, 1)
-    context.axes.hlines(x)
+    context.axes.hlines(context.series[:,0])
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-hline-default-colors")
+        context.canvas, "color-broadcast-hlines-default")
+
+@then(u'hlines can be rendered with one explicit color')
+def step_impl(context):
+    context.axes.hlines(context.series[:,0], color="red")
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-hlines-one-color")
+
+@then(u'hlines can be rendered with per-datum explicit colors')
+def step_impl(context):
+    context.axes.hlines(context.series[:,0], color=context.datum_colors[:,0])
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-hlines-per-datum-colors")
 
 @then(u'hlines can be rendered with palette colors')
 def step_impl(context):
-    x = numpy.linspace(0, 1)
-    context.axes.hlines(x, color=toyplot.color.brewer("Reds"))
+    context.axes.hlines(context.series[:,0], color=toyplot.color.brewer("Set1"))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-hline-palette-colors")
+        context.canvas, "color-broadcast-hlines-palette")
 
 @then(u'hlines can be rendered with colormap colors')
 def step_impl(context):
-    x = numpy.linspace(0, 1)
-    context.axes.hlines(x, color=toyplot.color.LinearMap(toyplot.color.brewer("Greens")))
+    context.axes.hlines(context.series[:,0], color=toyplot.color.LinearMap(toyplot.color.brewer("Set1")))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-hline-colormap-colors")
+        context.canvas, "color-broadcast-hlines-colormap")
 
-@then(u'hlines can be rendered with value colors')
+@then(u'hlines can be rendered with per-datum value colors')
 def step_impl(context):
-    x = numpy.linspace(0, 1)
-    context.axes.hlines(x, color=numpy.linspace(0, 1))
+    datum_values = numpy.arange(len(context.series[:,0]))
+    context.axes.hlines(context.series[:,0], color=datum_values)
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-hline-value-colors")
+        context.canvas, "color-broadcast-hlines-per-datum-values")
 
-@then(u'hlines can be rendered with value + palette colors')
+@then(u'hlines can be rendered with per-datum value + palette colors')
 def step_impl(context):
-    x = numpy.linspace(0, 1)
-    context.axes.hlines(x, color=(x, toyplot.color.brewer("Purples")))
+    datum_values = numpy.arange(len(context.series[:,0]))
+    context.axes.hlines(context.series[:,0], color=(datum_values, toyplot.color.brewer("Reds")))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-hline-value-palette-colors")
+        context.canvas, "color-broadcast-hlines-per-datum-values-palette")
 
-@then(u'hlines can be rendered with value + colormap colors')
+@then(u'hlines can be rendered with per-datum value + colormap colors')
 def step_impl(context):
-    x = numpy.linspace(0, 1)
-    context.axes.hlines(x, color=(x, toyplot.color.LinearMap(toyplot.color.brewer("Oranges"))))
+    datum_values = numpy.arange(len(context.series[:,0]))
+    context.axes.hlines(context.series[:,0], color=(datum_values, toyplot.color.LinearMap()))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-hline-value-colormap-colors")
+        context.canvas, "color-broadcast-hlines-per-datum-values-colormap")
+
 
 @then(u'vlines can be rendered with default colors')
 def step_impl(context):
-    y = numpy.linspace(0, 1)
-    context.axes.vlines(y)
+    context.axes.vlines(context.series[:,0])
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-vline-default-colors")
+        context.canvas, "color-broadcast-vlines-default")
+
+@then(u'vlines can be rendered with one explicit color')
+def step_impl(context):
+    context.axes.vlines(context.series[:,0], color="red")
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-vlines-one-color")
+
+@then(u'vlines can be rendered with per-datum explicit colors')
+def step_impl(context):
+    context.axes.vlines(context.series[:,0], color=context.datum_colors[:,0])
+    toyplot.testing.assert_canvas_equal(
+        context.canvas, "color-broadcast-vlines-per-datum-colors")
 
 @then(u'vlines can be rendered with palette colors')
 def step_impl(context):
-    y = numpy.linspace(0, 1)
-    context.axes.vlines(y, color=toyplot.color.brewer("Reds"))
+    context.axes.vlines(context.series[:,0], color=toyplot.color.brewer("Set1"))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-vline-palette-colors")
+        context.canvas, "color-broadcast-vlines-palette")
 
 @then(u'vlines can be rendered with colormap colors')
 def step_impl(context):
-    y = numpy.linspace(0, 1)
-    context.axes.vlines(y, color=toyplot.color.LinearMap(toyplot.color.brewer("Greens")))
+    context.axes.vlines(context.series[:,0], color=toyplot.color.LinearMap(toyplot.color.brewer("Set1")))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-vline-colormap-colors")
+        context.canvas, "color-broadcast-vlines-colormap")
 
-@then(u'vlines can be rendered with value colors')
+@then(u'vlines can be rendered with per-datum value colors')
 def step_impl(context):
-    y = numpy.linspace(0, 1)
-    context.axes.vlines(y, color=y)
+    datum_values = numpy.arange(len(context.series[:,0]))
+    context.axes.vlines(context.series[:,0], color=datum_values)
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-vline-value-colors")
+        context.canvas, "color-broadcast-vlines-per-datum-values")
 
-@then(u'vlines can be rendered with value + palette colors')
+@then(u'vlines can be rendered with per-datum value + palette colors')
 def step_impl(context):
-    y = numpy.linspace(0, 1)
-    context.axes.vlines(y, color=(y, toyplot.color.brewer("Purples")))
+    datum_values = numpy.arange(len(context.series[:,0]))
+    context.axes.vlines(context.series[:,0], color=(datum_values, toyplot.color.brewer("Reds")))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-vline-value-palette-colors")
+        context.canvas, "color-broadcast-vlines-per-datum-values-palette")
 
-@then(u'vlines can be rendered with value + colormap colors')
+@then(u'vlines can be rendered with per-datum value + colormap colors')
 def step_impl(context):
-    y = numpy.linspace(0, 1)
-    context.axes.vlines(y, color=(y, toyplot.color.LinearMap(toyplot.color.brewer("Oranges"))))
+    datum_values = numpy.arange(len(context.series[:,0]))
+    context.axes.vlines(context.series[:,0], color=(datum_values, toyplot.color.LinearMap()))
     toyplot.testing.assert_canvas_equal(
-        context.canvas, "color-vline-value-colormap-colors")
+        context.canvas, "color-broadcast-vlines-per-datum-values-colormap")
 
 

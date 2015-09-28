3ba40a320b1c9d61c83534e2d66c9fb93e421ca6
Timothy M. Shead <tshead@sandia.gov>
2015-09-28 15:49:04 -0600

    Add a function to generate Barabasi-Albert random graphs.

--- toyplot/generate.py
+++ toyplot/generate.py
@@ -37,3 +37,27 @@ def prufer_tree(sequence):
 
         return numpy.column_stack((sources, targets))
 
+def barabasi_albert_graph(n=300, m=2, seed=1234):
+    """Generate a graph using the preferential attachment model of Barabasi and Albert.
+    """
+    if m < 1 or m >= n:
+        raise ValueError()
+
+    generator = numpy.random.RandomState(seed=seed)
+
+    sources = []
+    targets = []
+
+    new_source = m
+    new_targets = numpy.arange(m)
+    repeated_nodes = numpy.array([], dtype="int64")
+    while new_source < n:
+        for new_target in new_targets:
+            sources.append(new_source)
+            targets.append(new_target)
+        repeated_nodes = numpy.append(repeated_nodes, new_targets)
+        repeated_nodes = numpy.append(repeated_nodes, numpy.repeat(new_source, m))
+        new_targets = generator.choice(repeated_nodes, size=m)
+        new_source += 1
+
+    return numpy.column_stack((sources, targets))

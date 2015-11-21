ba1834ef9afd14b1da76289ca2074bb91e744525
Timothy M. Shead <tshead@sandia.gov>
2015-11-21 13:59:42 -0700

    Start a new PDF backend based on the ReportLab library.

---
+++ toyplot/reportlab/__init__.py
@@ -0,0 +1,301 @@
+# Copyright 2014, Sandia Corporation. Under the terms of Contract
+# DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
+# rights in this software.
+
+"""Support functions for rendering using Cairo."""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import numpy
+import reportlab.pdfgen.canvas
+import toyplot.color
+import toyplot.units
+
+
+def render(svg, canvas):
+    """Render the SVG representation of a canvas to a Cairo canvas.
+
+    Parameters
+    ----------
+    svg: xml.etree.ElementTree.Element
+      SVG representation of a :class:`toyplot.canvas.Canvas` returned by
+      :func:`toyplot.svg.render()`.
+
+    canvas: cairo.Context
+      Cairo canvas that will be used to render the plot.
+    """
+    def get_fill(style):
+        if "fill" not in style:
+            return None
+
+        color = toyplot.color.css(style["fill"])
+        if color is None:
+            return color
+
+        fill_opacity = float(style.get("fill-opacity", 1.0))
+        opacity = float(style.get("opacity", 1.0))
+        return toyplot.color.rgba(
+            color["r"],
+            color["g"],
+            color["b"],
+            color["a"] * fill_opacity * opacity,
+            )
+
+    def get_stroke(style):
+        if "stroke" not in style:
+            return None
+
+        color = toyplot.color.css(style["stroke"])
+        if color is None:
+            return None
+
+        stroke_opacity = float(style.get("stroke-opacity", 1.0))
+        opacity = float(style.get("opacity", 1.0))
+        return toyplot.color.rgba(
+            color["r"],
+            color["g"],
+            color["b"],
+            color["a"] * stroke_opacity * opacity,
+            )
+
+    def set_fill_color(canvas, color):
+        canvas.setFillColorRGB(color["r"], color["g"], color["b"])
+        canvas.setFillAlpha(numpy.asscalar(color["a"]))
+
+    def set_stroke_color(canvas, color):
+        canvas.setStrokeColorRGB(color["r"], color["g"], color["b"])
+        canvas.setStrokeAlpha(numpy.asscalar(color["a"]))
+
+    def render_element(root, element, canvas, styles):
+        canvas.saveState()
+
+        current_style = {}
+        if len(styles):
+            current_style.update(styles[-1])
+
+        current_style.update(dict(
+            [declaration.split(":") for declaration in element.get("style", "").split(";") if declaration != ""]))
+        styles.append(current_style)
+
+        if "stroke-width" in current_style:
+            canvas.setLineWidth(float(current_style["stroke-width"]))
+
+        if "stroke-dasharray" in current_style:
+            canvas.setDash([float(length) for length in current_style["stroke-dasharray"].split(",")])
+
+        if current_style.get("visibility") != "hidden":
+
+            if "transform" in element.attrib:
+                for transformation in element.get("transform").split(")")[::1]:
+                    if transformation:
+                        type, arguments = transformation.split("(")
+                        arguments = arguments.split(",")
+                        if type.strip() == "translate":
+                            if len(arguments) == 2:
+                                canvas.translate(float(arguments[0]), float(arguments[1]))
+                        elif type.strip() == "rotate":
+                            if len(arguments) == 1:
+                                canvas.rotate(float(arguments[0]))
+                            if len(arguments) == 3:
+                                canvas.translate(float(arguments[1]), float(arguments[2]))
+                                canvas.rotate(float(arguments[0]))
+                                canvas.translate(-float(arguments[1]), -float(arguments[2]))
+
+            if element.tag == "svg":
+                if "background-color" in current_style:
+                    set_fill_color(canvas, toyplot.color.css(current_style["background-color"]))
+                    canvas.rect(
+                        0,
+                        0,
+                        float(element.get("width")[:-2]),
+                        float(element.get("height")[:-2]),
+                        stroke=0,
+                        fill=1,
+                        )
+                for child in element:
+                    render_element(root, child, canvas, styles)
+
+            elif element.tag == "g":
+                if element.get("clip-path", None) is not None:
+                    clip_id = element.get("clip-path")[5:-1]
+                    clip_path = root.find(".//*[@id='%s']" % clip_id)
+                    for child in clip_path:
+                        if child.tag == "rect":
+                            x = float(child.get("x"))
+                            y = float(child.get("y"))
+                            width = float(child.get("width"))
+                            height = float(child.get("height"))
+                            path = canvas.beginPath()
+                            path.moveTo(x, y)
+                            path.lineTo(x + width, y)
+                            path.lineTo(x + width, y + height)
+                            path.lineTo(x, y + height)
+                            path.close()
+                            canvas.clipPath(path, stroke=0, fill=1)
+                        else:
+                            toyplot.log.error("Unhandled clip tag: %s" % child.tag) # pragma: no cover
+
+                for child in element:
+                    render_element(root, child, canvas, styles)
+
+            elif element.tag == "clipPath":
+                pass
+
+            elif element.tag == "line":
+                stroke = get_stroke(current_style)
+                if stroke is not None:
+                    set_stroke_color(canvas, stroke)
+                    canvas.line(
+                        float(element.get("x1")),
+                        float(element.get("y1")),
+                        float(element.get("x2")),
+                        float(element.get("y2")),
+                        )
+            elif element.tag == "path":
+                stroke = get_stroke(current_style)
+                if stroke is not None:
+                    set_stroke_color(canvas, stroke)
+                    path = canvas.beginPath()
+                    commands = element.get("d").split()
+                    while len(commands):
+                        command = commands.pop(0)
+                        if command == "L":
+                            path.lineTo(
+                                float(commands.pop(0)), float(commands.pop(0)))
+                        elif command == "M":
+                            path.moveTo(
+                                float(commands.pop(0)), float(commands.pop(0)))
+                    canvas.drawPath(path)
+            elif element.tag == "polygon":
+                fill = get_fill(current_style)
+                if fill is not None:
+                    set_fill_color(canvas, fill)
+                stroke = get_stroke(current_style)
+                if stroke is not None:
+                    set_stroke_color(canvas, stroke)
+
+                points = [point.split(",") for point in element.get("points").split()]
+                path = canvas.beginPath()
+                for point in points[:1]:
+                    path.moveTo(float(point[0]), float(point[1]))
+                for point in points[1:]:
+                    path.lineTo(float(point[0]), float(point[1]))
+                path.close()
+                canvas.drawPath(path, stroke=stroke is not None, fill=fill is not None)
+            elif element.tag == "rect":
+                fill = get_fill(current_style)
+                if fill is not None:
+                    set_fill_color(canvas, fill)
+                stroke = get_stroke(current_style)
+                if stroke is not None:
+                    set_stroke_color(canvas, stroke)
+
+                x = float(element.get("x"))
+                y = float(element.get("y"))
+                width = float(element.get("width"))
+                height = float(element.get("height"))
+                canvas.rect(x, y, width, height, stroke=stroke is not None, fill=fill is not None)
+            elif element.tag == "circle":
+                fill = get_fill(current_style)
+                if fill is not None:
+                    set_fill_color(canvas, fill)
+                stroke = get_stroke(current_style)
+                if stroke is not None:
+                    set_stroke_color(canvas, stroke)
+
+                cx = float(element.get("cx"))
+                cy = float(element.get("cy"))
+                r = float(element.get("r"))
+                canvas.circle(cx, cy, r, stroke=stroke is not None, fill=fill is not None)
+#            elif element.tag == "text":
+#                if element.text is not None and element.text != "":
+#                    pangocairo_canvas = pangocairo.CairoContext(canvas)
+#                    pangocairo_canvas.set_antialias(cairo.ANTIALIAS_SUBPIXEL)
+#                    layout = pangocairo_canvas.create_layout()
+#                    layout.set_text(element.text)
+#
+#                    font_description = pango.FontDescription()
+#                    if "font-family" in current_style:
+#                        font_description.set_family(
+#                            current_style["font-family"])
+#                    if "font-weight" in current_style:
+#                        font_description.set_weight(
+#                            pango.WEIGHT_BOLD if current_style["font-weight"] == "bold" else pango.WEIGHT_NORMAL)
+#                    if "font-size" in current_style:
+#                        size = current_style["font-size"].strip()
+#                        if size[-2:] == "px":
+#                            # Convert CSS pixels to points
+#                            size = int(
+#                                pango.SCALE * float(size[:-2]) * 72.0 / 96.0)
+#                        else:
+#                            raise ValueError("font-size must use pixel units") # pragma: no cover
+#                        font_description.set_size(size)
+#                    layout.set_font_description(font_description)
+#
+#                    ink_extents, logical_extents = layout.get_pixel_extents()
+#
+#                    x = float(element.get("x"))
+#                    y = float(element.get("y"))
+#
+#                    text_anchor = current_style.get("text-anchor", "start")
+#                    if text_anchor == "start":
+#                        pass
+#                    elif text_anchor == "middle":
+#                        x -= logical_extents[2] * 0.5
+#                    elif text_anchor == "end":
+#                        x -= logical_extents[2]
+#
+#                    dx = toyplot.units.convert(
+#                        element.get("dx", 0), "px", default="px")
+#                    x += dx
+#
+#                    alignment_baseline = current_style.get(
+#                        "alignment-baseline", "middle")
+#                    if alignment_baseline == "hanging":
+#                        y -= ink_extents[1]
+#                    elif alignment_baseline == "central":
+#                        y -= ink_extents[3] * 0.5
+#                    elif alignment_baseline == "middle":
+#                        y -= logical_extents[3] * 0.5
+#                    elif alignment_baseline == "alphabetic":
+#                        layout_iter = layout.get_iter()
+#                        y -= layout_iter.get_baseline() / pango.SCALE
+#                    else:
+#                        raise ValueError(
+#                            "Unsupported alignment-baseline: %s" %
+#                            alignment_baseline) # pragma: no cover
+#
+#                    baseline_shift = current_style.get(
+#                        "baseline-shift", "0").strip()
+#                    if baseline_shift[-1] == "%":
+#                        y -= ((ink_extents[3] + logical_extents[3])
+#                              * 0.5) * float(baseline_shift[:-1]) * 0.01
+#                    else:
+#                        y -= float(baseline_shift)
+#
+#                    canvas.new_path()
+#                    canvas.move_to(x, y)
+#                    pangocairo_canvas.update_layout(layout)
+#                    pangocairo_canvas.layout_path(layout)
+#
+#                    fill = get_fill(current_style)
+#                    if fill is not None:
+#                        canvas.set_source_rgba(*fill)
+#                        canvas.fill_preserve()
+#                    stroke = get_stroke(current_style)
+#                    if stroke is not None:
+#                        canvas.set_source_rgba(*stroke)
+#                        canvas.stroke()
+#                    canvas.new_path()
+            elif element.tag in ["title"]:
+                pass
+            elif element.tag in ["text"]:
+                pass
+            else:
+                raise Exception("unhandled tag: %s" % element.tag) # pragma: no cover
+
+        styles.pop()
+        canvas.restoreState()
+
+    render_element(svg, svg, canvas, [])
---
+++ toyplot/reportlab/pdf.py
@@ -0,0 +1,75 @@
+# Copyright 2014, Sandia Corporation. Under the terms of Contract
+# DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
+# rights in this software.
+
+"""Generate PDF documents using ReportLab."""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import reportlab.pdfgen.canvas
+import toyplot.svg
+import toyplot.reportlab
+
+
+try:
+    import cStringIO as StringIO
+except:  # pragma: no cover
+    import StringIO
+
+
+def render(canvas, fobj=None, width=None, height=None, scale=None):
+    """Render the PDF representation of a canvas using ReportLab.
+
+    Because the canvas dimensions are specified explicitly at creation time, they
+    map directly to real-world units in the output PDF image.  Use one of
+    `width`, `height`, or `scale` to override this behavior.
+
+    Parameters
+    ----------
+    canvas: :class:`toyplot.canvas.Canvas`
+      Canvas to be rendered.
+    fobj: file-like object or string
+      The file to write.  Use a string filepath to write data directly to disk.
+      If `None` (the default), the PDF data will be returned to the caller
+      instead.
+    width: number, string, or (number, string) tuple, optional
+      Specify the width of the output image with optional units.  If the units
+      aren't specified, defaults to points.  See :ref:`units` for details on
+      unit conversion in Toyplot.
+    height: number or (number, string) tuple, optional
+      Specify the height of the output image with optional units.  If the units
+      aren't specified, defaults to points.  See :ref:`units` for details on
+      unit conversion in Toyplot.
+    scale: number, optional
+      Scales the output `canvas` by the given ratio.
+
+    Returns
+    -------
+    pdf: PDF data, or `None`
+      PDF representation of `canvas`, or `None` if the caller specifies the
+      `fobj` parameter.
+
+    Examples
+    --------
+
+    >>> toyplot.reportlab.pdf.render(canvas, "figure-1.pdf", width=(4, "inches"))
+    """
+    svg = toyplot.svg.render(canvas)
+    scale = canvas._point_scale(width=width, height=height, scale=scale)
+    if fobj is None:
+        stream = StringIO.StringIO()
+        surface = reportlab.pdfgen.canvas.Canvas(
+            stream, pagesize=(scale * canvas.width, scale * canvas.height))
+    else:
+        surface = reportlab.pdfgen.canvas.Canvas(
+            fobj, pagesize=(scale * canvas.width, scale * canvas.height))
+    surface.translate(0, scale * canvas.height)
+    surface.scale(1, -1)
+    surface.scale(scale, scale)
+    toyplot.reportlab.render(svg, surface)
+    surface.showPage()
+    surface.save()
+    if fobj is None:
+        return stream.getvalue()
+

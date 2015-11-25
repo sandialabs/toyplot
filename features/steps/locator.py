cc7bf7949e9eab989292617f52b679beecb7d88a
Timothy M. Shead <tshead@sandia.gov>
2015-11-25 14:05:06 -0700

    Add regression tests for the timestamp locator.

--- features/steps/locator.py
+++ features/steps/locator.py
@@ -1,5 +1,6 @@
 from behave import *
 
+import arrow
 import numpy
 import toyplot.locator
 import toyplot.testing
@@ -90,5 +91,95 @@ def step_impl(context):
     axes.y.ticks.locator = toyplot.locator.Null()
     axes.plot(context.x, context.y)
     toyplot.testing.assert_canvas_equal(canvas, "tick-locator-null")
+
+@given(u'five years of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2021, 1, 1).timestamp
+
+@given(u'one year of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2017, 1, 1).timestamp
+
+@given(u'six months of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 7, 1).timestamp
+
+@given(u'one month of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 2, 1).timestamp
+
+@given(u'one week of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 8).timestamp
+
+@given(u'one day of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 2).timestamp
+
+@given(u'six hours of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 1, 6).timestamp
+
+@given(u'two hours of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 1, 2).timestamp
+
+@given(u'one hour of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 1, 1).timestamp
+
+@given(u'thirty minutes of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 1, 0, 30).timestamp
+
+@given(u'five minutes of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 1, 0, 5).timestamp
+
+@given(u'two minutes of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 1, 0, 2).timestamp
+
+@given(u'one minute of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 1, 0, 1, 0).timestamp
+
+@given(u'thirty seconds of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 1, 0, 0, 30).timestamp
+
+@given(u'five seconds of timestamp data')
+def step_impl(context):
+    context.timestamp_begin = arrow.get(2016, 1, 1).timestamp
+    context.timestamp_end = arrow.get(2016, 1, 1, 0, 0, 5).timestamp
+
+@given(u'a visualization using the timestamp locator')
+def step_impl(context):
+    numpy.random.seed(1234)
+    timestamps = numpy.random.uniform(context.timestamp_begin, context.timestamp_end, size=100)
+
+    context.canvas = toyplot.Canvas(width=800, height=200)
+    axes = context.canvas.axes(xmin=context.timestamp_begin, xmax=context.timestamp_end, yshow=False)
+    axes.x.ticks.locator = toyplot.locator.Timestamp(count=7)
+    axes.x.ticks.show = True
+    axes.scatterplot(timestamps, numpy.zeros_like(timestamps), marker="|", size=200)
+
+@then(u'the generated figure will match {reference}')
+def step_impl(context, reference):
+    toyplot.testing.assert_canvas_equal(context.canvas, reference)
 
 
--- features/tick-locators.feature
+++ features/tick-locators.feature
@@ -1,16 +1,39 @@
 Feature: Tick locators
-  Scenario Outline: Tick locators
-    Given sample sin wave data
-    Then the data can be rendered <phrase>
+    Scenario Outline: Tick locators
+        Given sample sin wave data
+        Then the data can be rendered <phrase>
 
-    Examples:
-      | phrase                                       |
-      | with default ticks                           |
-      | with ticks evenly spread across the domain   |
-      | with explicit locations                      |
-      | with explicit locations and explicit labels  |
-      | with explicit labels                         |
-      | with ticks identified by heckbert            |
-      | with ticks identified by optimization        |
-      | without ticks                                |
+        Examples:
+            | phrase                                       |
+            | with default ticks                           |
+            | with ticks evenly spread across the domain   |
+            | with explicit locations                      |
+            | with explicit locations and explicit labels  |
+            | with explicit labels                         |
+            | with ticks identified by heckbert            |
+            | with ticks identified by optimization        |
+            | without ticks                                |
+
+    Scenario Outline: Tick locators
+        Given <period> of timestamp data
+        And a visualization using the timestamp locator
+        Then the generated figure will match <reference>
+
+        Examples:
+            | period                                       | reference                        |
+            | five years                                   | tick-locator-timestamp-year      |
+            | one year                                     | tick-locator-timestamp-quarter   |
+            | six months                                   | tick-locator-timestamp-month     |
+            | one month                                    | tick-locator-timestamp-week      |
+            | one week                                     | tick-locator-timestamp-day       |
+            | one day                                      | tick-locator-timestamp-4-hour    |
+            | six hours                                    | tick-locator-timestamp-hour      |
+            | two hours                                    | tick-locator-timestamp-15-minute |
+            | one hour                                     | tick-locator-timestamp-10-minute |
+            | thirty minutes                               | tick-locator-timestamp-5-minute  |
+            | five minutes                                 | tick-locator-timestamp-minute    |
+            | two minutes                                  | tick-locator-timestamp-15-second |
+            | one minute                                   | tick-locator-timestamp-10-second |
+            | thirty seconds                               | tick-locator-timestamp-5-second  |
+            | five seconds                                 | tick-locator-timestamp-second    |
 

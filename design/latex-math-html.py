3368977f8d0e3796ff98054866163f9630a3a812
Timothy M. Shead <tshead@sandia.gov>
2015-12-18 14:37:05 -0700

    Expanding the MathJax / HTML labelling example.

--- design/latex-math-html.py
+++ design/latex-math-html.py
@@ -1,6 +1,74 @@
 import xml.etree.ElementTree as xml
 
-equation = "E=\\frac{m_1v^2}{2}"
+markup = "The answer is: \\(E=\\frac{m_1v^2}{2}\\)"
+angle = -30
+align = None
+ashift = None
+bshift = -16
+
+def draw_text(parent, markup, ax, ay, angle=None, align=None, ashift=None, bshift=None, twidth=None, theight=None):
+    if angle is None:
+        angle = 0
+
+    if align is None:
+        if angle > 0:
+            align = "right"
+        elif angle < 0:
+            align = "left"
+        else:
+            align = "center"
+
+    if ashift is None:
+        if angle > 0:
+            ashift = -10
+        elif angle < 0:
+            ashift = 10
+        else:
+            ashift = 0
+
+    if bshift is None:
+        bshift = 0
+
+    if twidth is None:
+        twidth = 200
+
+    if theight is None:
+        theight = 200
+
+    if align == "left":
+        x = ax
+        y = ay
+    elif align == "center":
+        x = ax - twidth / 2
+        y = ay
+    elif align == "right":
+        x = ax - twidth
+        y = ay
+
+    xml.SubElement(parent, "circle", cx=repr(ax), cy=repr(ay), r="1")
+
+    fo = xml.SubElement(
+        parent,
+        "foreignObject",
+        x=repr(x),
+        y=repr(y),
+        width=repr(twidth),
+        height=repr(theight),
+        transform="rotate(%s, %s, %s) translate(%s, %s)" % (-angle, ax, ay, ashift, bshift)
+        )
+
+    container = xml.SubElement(
+        fo,
+        "div",
+        xmlns="http://www.w3.org/1999/xhtml",
+        style="text-align: %s" % align,
+        )
+
+    try:
+        container.append(xml.fromstring(markup))
+    except:
+        container.text = markup
+
 
 root = xml.Element(
     "div",
@@ -25,25 +93,8 @@ svg = xml.SubElement(
     preserveAspectRatio="xMidYMid meet",
     )
 
-xml.SubElement(
-    svg,
-    "circle",
-    cx="200",
-    cy="200",
-    r="2",
-    )
-
-fo = xml.SubElement(
-    svg,
-    "foreignObject",
-    x="100",
-    y="200",
-    width="200",
-    height="100",
-    )
-
-fo.text = "Displaying \\(%s\\) for your enjoyment." % equation
-#fo.text = "$$%s$$" % equation
+draw_text(parent=svg, markup=markup, ax=200, ay=200, angle=angle, align=align, ashift=ashift, bshift=bshift)
+draw_text(parent=svg, markup="<p>0.3<br></br><span style='font-size:75%'>(0.8, 0.6, 0.2)</span></p>", ax=200, ay=300)
 
 with open("latex-math-html.html", "wb") as stream:
     stream.write(xml.tostring(root, method="html"))

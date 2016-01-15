b69ec131c87f1f902de53718288a29aaea4200c2
Timothy M. Shead <tshead@sandia.gov>
2016-01-14 22:00:08 -0700

    Add a regression test using categorical color maps with color scales.

--- features/color-scale.feature
+++ features/color-scale.feature
@@ -3,28 +3,32 @@ Feature: Color scale
     Given a default canvas
     And a <colormap>
     Then a <scale> can be added to the canvas
+    And the visualization should match the <reference> reference image
 
     Examples:
-      | colormap                      | scale                                                 |
-      | explicit linear color map     | vertical color scale                                  |
-      | explicit linear color map     | diagonal color scale                                  |
+      | colormap            | scale                | reference |
+      | linear colormap     | vertical color scale | color-scale-canvas-vertical |
+      | linear colormap     | diagonal color scale | color-scale-canvas-diagonal |
 
   Scenario Outline: Canvas color scales
     Given a default canvas
     And a set of default axes
     And a <colormap>
     Then a <scale> can be added to the axes
+    And the visualization should match the <reference> reference image
 
     Examples:
-      | colormap                      | scale                                                 |
-      | explicit linear color map     | color scale                                           |
+      | colormap                 | scale          | reference |
+      | linear colormap          | color scale    | color-scale-axes-linear-colormap      |
+      | categorical colormap     | color scale    | color-scale-axes-categorical-colormap |
 
   Scenario Outline: Matrix visualization color scales
     Given a default canvas
     Then a <scale> can be added to a matrix visualization
+    And the visualization should match the <reference> reference image
 
     Examples:
-      | scale                                          |
-      | color scale with default colormap              |
-      | color scale with explicit colormap             |
+      | scale                               | reference |
+      | color scale with default colormap   | color-scale-matrix-default-colormap |
+      | color scale with linear colormap    | color-scale-matrix-linear-colormap  |
 
--- features/steps/color-scale.py
+++ features/steps/color-scale.py
@@ -1,21 +1,23 @@
 from behave import *
 
 import numpy
-import toyplot.testing
+import toyplot.color
 
-@given(u'a explicit linear color map')
+@given(u'a linear colormap')
 def step_impl(context):
     context.colormap = toyplot.color.LinearMap(toyplot.color.brewer("BlueRed"), domain_min=0, domain_max=1)
+
+@given(u'a categorical colormap')
+def step_impl(context):
+    context.colormap = toyplot.color.CategoricalMap()
 
 @then(u'a vertical color scale can be added to the canvas')
 def step_impl(context):
     context.canvas.color_scale(context.colormap, x1=100, x2=100, y1="-10%", y2="10%")
-    toyplot.testing.assert_canvas_equal(context.canvas, "color-scale-canvas-vertical")
 
 @then(u'a diagonal color scale can be added to the canvas')
 def step_impl(context):
     context.canvas.color_scale(context.colormap, x1=100, x2=-100, y1=-100, y2=100)
-    toyplot.testing.assert_canvas_equal(context.canvas, "color-scale-canvas-diagonal")
 
 @given(u'a set of default axes')
 def step_impl(context):
@@ -24,21 +26,17 @@ def step_impl(context):
 @then(u'a color scale can be added to the axes')
 def step_impl(context):
     context.axes.color_scale(context.colormap)
-    toyplot.testing.assert_canvas_equal(context.canvas, "color-scale-axes-default")
 
 @then(u'a color scale with default colormap can be added to a matrix visualization')
 def step_impl(context):
     numpy.random.seed(1234)
     values = numpy.random.normal(size=(10, 10))
     context.canvas.matrix(values, colorshow=True)
-    toyplot.testing.assert_canvas_equal(context.canvas, "color-scale-matrix-default")
 
-@then(u'a color scale with explicit colormap can be added to a matrix visualization')
+@then(u'a color scale with linear colormap can be added to a matrix visualization')
 def step_impl(context):
     numpy.random.seed(1234)
     values = numpy.random.normal(size=(10, 10))
     colormap = toyplot.color.LinearMap(toyplot.color.brewer("BlueGreenBrown"), domain_min=-2, domain_max=2)
     context.canvas.matrix((values, colormap), colorshow=True)
-    toyplot.testing.assert_canvas_equal(context.canvas, "color-scale-matrix-explicit")
-
 

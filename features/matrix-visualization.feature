83e8733dc101dad2340b60e9dee3761d9a88cb0f
Timothy M. Shead <tshead@sandia.gov>
2016-01-16 16:25:39 -0700

    Add a regression test for specifying matrix visualization locators.

--- features/matrix-visualization.feature
+++ features/matrix-visualization.feature
@@ -1,8 +1,14 @@
 Feature: Matrix visualization
-    Scenario: Default matrix visualization
+    Scenario Outline: Matrix visualization
         Given a matrix
-        Then a default matrix visualization can be created with the convenience API
-        And a default matrix visualization can be created with the standard API
-        And a matrix visualization can be created with a custom palette
-        And a matrix visualization can be created with a custom colormap
-        And a matrix visualization can be created with custom labels
+        And a matrix visualization <scenario>
+        Then the visualization should match the <reference> reference image
+
+        Examples:
+            | scenario | reference |
+            | created with the convenience API | matrix-default |
+            | created with the standard API | matrix-default |
+            | created with a custom palette | matrix-custom-palette |
+            | created with a custom colormap | matrix-custom-colormap |
+            | created with custom labels | matrix-custom-labels |
+            | created with right and bottom index locators | matrix-right-bottom-locators |
---
+++ features/steps/matrix-visualization.py
@@ -0,0 +1,42 @@
+from behave import *
+
+import numpy
+import toyplot.locator
+
+@given(u'a matrix')
+def step_impl(context):
+    numpy.random.seed(1234)
+    context.matrix = numpy.random.normal(size=(10, 10))
+
+@given(u'a matrix visualization created with the convenience API')
+def step_impl(context):
+    context.canvas, table = toyplot.matrix(context.matrix)
+
+@given(u'a matrix visualization created with the standard API')
+def step_impl(context):
+    context.canvas = toyplot.Canvas()
+    table = context.canvas.matrix(context.matrix)
+
+@given(u'a matrix visualization created with a custom palette')
+def step_impl(context):
+    palette = toyplot.color.brewer("BlueGreenBrown")
+    context.canvas = toyplot.Canvas()
+    table = context.canvas.matrix((context.matrix, palette))
+
+@given(u'a matrix visualization created with a custom colormap')
+def step_impl(context):
+    palette = toyplot.color.brewer("BlueGreenBrown")
+    colormap = toyplot.color.LinearMap(palette, domain_min=-1, domain_max=1)
+    context.canvas = toyplot.Canvas()
+    table = context.canvas.matrix((context.matrix, colormap))
+
+@given(u'a matrix visualization created with custom labels')
+def step_impl(context):
+    context.canvas = toyplot.Canvas()
+    table = context.canvas.matrix(context.matrix, tlabel="Top", blabel="Bottom", llabel="Left", rlabel="Right", rshow=True, bshow=True)
+
+@given(u'a matrix visualization created with right and bottom index locators')
+def step_impl(context):
+    context.canvas = toyplot.Canvas()
+    table = context.canvas.matrix(context.matrix, rlocator=toyplot.locator.Integer(), blocator=toyplot.locator.Integer())
+

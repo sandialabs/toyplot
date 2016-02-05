a17977fd6b10967ba0fef1b919c14317d564fd90
Timothy M. Shead <tshead@sandia.gov>
2016-02-05 00:15:11 -0700

    Attempting to make some CSS tweaks to the readthedocs.org theme.

--- docs/conf.py
+++ docs/conf.py
@@ -155,6 +155,14 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_context = {
+        'css_files': [
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+            '_static/toyplot.css',
+        ],
+    }
 
 # otherwise, readthedocs.org uses their theme by default, so no need to
 # specify it

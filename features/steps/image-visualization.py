cd5cfa1549d64d4407effa6fe8a8adf0687b70e3
Timothy M. Shead <tshead@sandia.gov>
2016-04-12 13:18:56 -0600

    First iteration of working image visualization tests.

--- features/steps/image-visualization.py
+++ features/steps/image-visualization.py
@@ -4,30 +4,46 @@
 
 from behave import *
 
+import nose.tools
+import numpy
 import os
 import skimage.io
 import toyplot
 
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-art_dir = os.path.join(root_dir, "artwork")
+art_dir = os.path.abspath(os.path.dirname(__file__))
 
-@given(u'a 1 bit L image')
+@given(u'a numpy 1 bit L image')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a 1 bit L image')
+    context.image = skimage.io.imread(os.path.join(art_dir, "toyplot-8-L.png"))
+    context.image = context.image > 128
+    nose.tools.assert_equal(context.image.shape, (256, 256))
+    nose.tools.assert_equal(context.image.dtype, "bool")
 
-@given(u'an 8 bit L image')
+@given(u'a numpy 8 bit L image')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given an 8 bit L image')
+    context.image = skimage.io.imread(os.path.join(art_dir, "toyplot-8-L.png"))
+    nose.tools.assert_equal(context.image.shape, (256, 256))
+    nose.tools.assert_equal(context.image.dtype, "uint8")
 
-@given(u'an 8 bit RGB image')
+@given(u'a numpy 8 bit RGB image')
 def step_impl(context):
-    context.image = skimage.io.imread(os.path.join(art_dir, "toyplot-small.png"))[:,:,:3]
-    assert(context.image.shape == (256, 256, 3))
+    context.image = skimage.io.imread(os.path.join(art_dir, "toyplot-8-RGB.png"))
+    nose.tools.assert_equal(context.image.shape, (256, 256, 3))
+    nose.tools.assert_equal(context.image.dtype, "uint8")
 
-@given(u'an 8 bit RGBA image')
+@given(u'a numpy 8 bit RGBA image')
 def step_impl(context):
-    context.image = skimage.io.imread(os.path.join(art_dir, "toyplot-small.png"))
-    assert(context.image.shape == (256, 256, 4))
+    context.image = skimage.io.imread(os.path.join(art_dir, "toyplot-8-RGBA.png"))
+    nose.tools.assert_equal(context.image.shape, (256, 256, 4))
+    nose.tools.assert_equal(context.image.dtype, "uint8")
+
+@given(u'a PIL 8 bit RGBA image')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given a PIL 8 bit RGBA image')
+
+@given(u'a canvas background color')
+def step_impl(context):
+    context.canvas.style = {"background-color":"lightgray"}
 
 @when(u'the image is added to the canvas')
 def step_impl(context):

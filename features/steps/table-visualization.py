5a2fd237b2de0b2afab7ce4eac080a16de1108a1
Timothy M. Shead <tshead@sandia.gov>
2016-06-20 09:45:55 -0600

    Don't use deprecated API in regression tests.

--- features/steps/table-visualization.py
+++ features/steps/table-visualization.py
@@ -32,125 +32,125 @@ def step_impl(context):
 
 @then(u'the table can be rendered with header styles')
 def step_impl(context):
-    context.table_axes.header.column(1).lstyle = {"fill": "red"}
+    context.table_axes.top.column[1].lstyle = {"fill": "red"}
 
 
 @then(u'the table can be rendered with column styles')
 def step_impl(context):
-    context.table_axes.column(1).lstyle = {"fill": "red"}
+    context.table_axes.cells.column[1].lstyle = {"fill": "red"}
 
 
 @then(u'the table can be rendered with row styles')
 def step_impl(context):
-    context.table_axes.row(1).lstyle = {"fill": "green"}
+    context.table_axes.cells.row[1].lstyle = {"fill": "green"}
 
 
 @then(u'the table can be rendered with cell styles')
 def step_impl(context):
-    context.table_axes.cell(1, 1).lstyle = {"fill": "blue"}
+    context.table_axes.cells.cell[1, 1].lstyle = {"fill": "blue"}
 
 
 @then(u'the table can be rendered and row styles override column styles')
 def step_impl(context):
-    context.table_axes.column(1).lstyle = {"fill": "red"}
-    context.table_axes.row(1).lstyle = {"fill": "green"}
+    context.table_axes.cells.column[1].lstyle = {"fill": "red"}
+    context.table_axes.cells.row[1].lstyle = {"fill": "green"}
 
 
 @then(u'the table can be rendered and cell styles override row styles')
 def step_impl(context):
-    context.table_axes.row(1).lstyle = {"fill": "green"}
-    context.table_axes.cell(1, 1).lstyle = {"fill": "blue"}
+    context.table_axes.cells.row[1].lstyle = {"fill": "green"}
+    context.table_axes.cells.cell[1, 1].lstyle = {"fill": "blue"}
 
 
 @then(u'the table can be rendered and cell styles override column styles')
 def step_impl(context):
-    context.table_axes.column(1).lstyle = {"fill": "red"}
-    context.table_axes.cell(1, 1).lstyle = {"fill": "blue"}
+    context.table_axes.cells.column[1].lstyle = {"fill": "red"}
+    context.table_axes.cells.cell[1, 1].lstyle = {"fill": "blue"}
 
 
 @then(u'the table can be rendered with extra horizontal lines')
 def step_impl(context):
-    context.table_axes.grid.hlines[0, ...] = "single"
+    context.table_axes.cells.grid.hlines[0, ...] = "single"
 
 
 @then(u'the table can be rendered with extra vertical lines')
 def step_impl(context):
-    context.table_axes.grid.vlines[..., 1] = "single"
+    context.table_axes.cells.grid.vlines[..., 1] = "single"
 
 
 @then(u'the table can be rendered with a full grid')
 def step_impl(context):
-    context.table_axes.grid.hlines[...] = "single"
-    context.table_axes.grid.vlines[...] = "single"
+    context.table_axes.cells.grid.hlines[...] = "single"
+    context.table_axes.cells.grid.vlines[...] = "single"
 
 
 @then(u'the table can be rendered with grid styles')
 def step_impl(context):
-    context.table_axes.grid.style = {"stroke": "lightgray"}
-    context.table_axes.grid.hlines[...] = "single"
-    context.table_axes.grid.vlines[...] = "single"
+    context.table_axes.cells.grid.style = {"stroke": "lightgray"}
+    context.table_axes.cells.grid.hlines[...] = "single"
+    context.table_axes.cells.grid.vlines[...] = "single"
 
 
 @then(u'the table can be rendered with doubled lines')
 def step_impl(context):
-    context.table_axes.grid.hlines[1, ...] = "double"
-    context.table_axes.grid.vlines[..., 1] = "double"
+    context.table_axes.cells.grid.hlines[1, ...] = "double"
+    context.table_axes.cells.grid.vlines[..., 1] = "double"
 
 
 @then(u'the table can be rendered with custom doubled line separation')
 def step_impl(context):
-    context.table_axes.grid.hlines[1, ...] = "double"
-    context.table_axes.grid.vlines[..., 1] = "double"
-    context.table_axes.grid.separation = 4
+    context.table_axes.cells.grid.hlines[1, ...] = "double"
+    context.table_axes.cells.grid.vlines[..., 1] = "double"
+    context.table_axes.cells.grid.separation = 4
 
 
 @then(u'the table can be rendered with column offsets')
 def step_impl(context):
-    context.table_axes.column(0).column_offset = -50
-    context.table_axes.column(2).column_offset = 50
+    context.table_axes.cells.column[0].lstyle = {"-toyplot-anchor-shift": -50}
+    context.table_axes.cells.column[2].lstyle = {"-toyplot-anchor-shift": 50}
 
 
 @then(u'the table can be rendered with custom header content')
 def step_impl(context):
-    context.table_axes.header.cell(0, 1).data = "My Column"
+    context.table_axes.top.cell[0, 1].data = "My Column"
 
 
 @then(u'the table can be rendered with custom cell content')
 def step_impl(context):
-    context.table_axes.cell(1, 1).data = "My Cell"
+    context.table_axes.cells.cell[1, 1].data = "My Cell"
 
 
 @then(u'the table can be rendered with embedded plots')
 def step_impl(context):
     numpy.random.seed(1234)
-    context.table_axes.body.cell(0, 3).axes().plot(numpy.sin(numpy.linspace(0, 10)))
-    context.table_axes.body.cell(1, 3).axes().bars(
+    context.table_axes.body.cell[0, 3].axes().plot(numpy.sin(numpy.linspace(0, 10)))
+    context.table_axes.body.cell[1, 3].axes().bars(
         numpy.random.uniform(0.1, 1, size=10))
-    context.table_axes.body.cell(2, 3).axes().bars(numpy.random.choice(
+    context.table_axes.body.cell[2, 3].axes().bars(numpy.random.choice(
         [-1, 1], size=30), color=numpy.random.choice(["red", "blue"], size=30))
-    context.table_axes.body.cell(3, 3, rowspan=2).axes().fill(
+    context.table_axes.body.cell[3:5, 3].axes().fill(
         3 + numpy.cos(numpy.linspace(0, 5)) + numpy.sin(numpy.linspace(0, 20)))
 
 
 @then(u'the table can be rendered with custom column widths')
 def step_impl(context):
-    context.table_axes.column(0).width = 50
-    context.table_axes.column(2).width = 250
+    context.table_axes.cells.column[0].width = 50
+    context.table_axes.cells.column[2].width = 250
 
 
 @then(u'the table can be rendered with left justification')
 def step_impl(context):
-    context.table_axes.column(0).align = "left"
+    context.table_axes.cells.column[0].align = "left"
 
 
 @then(u'the table can be rendered with center justification')
 def step_impl(context):
-    context.table_axes.column(2).align = "center"
+    context.table_axes.cells.column[2].align = "center"
 
 
 @then(u'the table can be rendered with right justification')
 def step_impl(context):
-    context.table_axes.column(2).align = "right"
+    context.table_axes.cells.column[2].align = "right"
 
 
 @then(u'the table can be rendered with a label')
@@ -161,16 +161,16 @@ def step_impl(context):
 @then(u'the table can be rendered with multiple embedded axes in merged cells')
 def step_impl(context):
     numpy.random.seed(1234)
-    context.table_axes.body.column(2).merge().axes().bars(numpy.random.random(20), along="y")
-    context.table_axes.body.column(3).merge().axes().bars(numpy.random.random(20), along="y")
+    context.table_axes.body.column[2].merge().axes().bars(numpy.random.random(20), along="y")
+    context.table_axes.body.column[3].merge().axes().bars(numpy.random.random(20), along="y")
 
 
 @then(u'the table can be rendered with real world units')
 def step_impl(context):
-    context.table_axes.grid.hlines[...] = "single"
-    context.table_axes.grid.vlines[...] = "single"
-    context.table_axes.column(0).width = (1, "cm")
-    context.table_axes.row(0).height = "1cm"
+    context.table_axes.cells.grid.hlines[...] = "single"
+    context.table_axes.cells.grid.vlines[...] = "single"
+    context.table_axes.cells.column[0].width = (1, "cm")
+    context.table_axes.cells.row[0].height = "1cm"
 
 @then(u'an instance of toyplot.coordinates.Table can be rendered without a header')
 def step_impl(context):
@@ -192,6 +192,6 @@ def step_impl(context):
 @given(u'an instance of toyplot.coordinates.Table with 4 rows and 3 columns')
 def step_impl(context):
     context.table_axes = context.canvas.table(rows=4, columns=3)
-    context.table_axes.grid.hlines[...] = "single"
-    context.table_axes.grid.vlines[...] = "single"
+    context.table_axes.cells.grid.hlines[...] = "single"
+    context.table_axes.cells.grid.vlines[...] = "single"
 

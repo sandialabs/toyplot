100c8d3c66ca3388714844e6712af8acb0b8a2e8
Timothy M. Shead <tshead@sandia.gov>
2016-10-28 21:47:09 -0600

    Add toyplot.font.Library for managing collections of font information.

--- toyplot/font.py
+++ toyplot/font.py
@@ -11,52 +11,9 @@ import reportlab.pdfbase.pdfmetrics
 
 import toyplot.units
 
-def family(style):
-    """Extract the name of a standard PDF font family from a CSS style dict.
-
-    Parameters
-    ----------
-    style: dict containing CSS key-value pairs.
-    """
-    if "font-family" not in style:
-        raise ValueError("No font family specified: %s" % style)
-
-    bold = True if style.get("font-weight", "") == "bold" else False
-    italic = True if style.get("font-style", "") == "italic" else False
-    for font_family in style["font-family"].split(","):
-        font_family = font_family.lower()
-        if font_family in family.substitutions:
-            font_family = family.substitutions[font_family]
-            return family.font_table[(font_family, bold, italic)]
-
-    raise ValueError("Unknown font family: %s" % style) # pragma: no cover
-
-family.font_table = {
-    ("courier", False, False): "Courier",
-    ("courier", True, False): "Courier-Bold",
-    ("courier", False, True): "Courier-Oblique",
-    ("courier", True, True): "Courier-BoldOblique",
-    ("helvetica", False, False): "Helvetica",
-    ("helvetica", True, False): "Helvetica-Bold",
-    ("helvetica", False, True): "Helvetica-Oblique",
-    ("helvetica", True, True): "Helvetica-BoldOblique",
-    ("times", False, False): "Times-Roman",
-    ("times", True, False): "Times-Bold",
-    ("times", False, True): "Times-Italic",
-    ("times", True, True): "Times-BoldItalic",
-    }
-
-family.substitutions = {
-    "courier": "courier",
-    "helvetica": "helvetica",
-    "monospace": "courier",
-    "sans-serif": "helvetica",
-    "serif": "times",
-    "times": "times",
-    }
-
-class Metrics(object):
-    """Base class for objects that can return font metrics information."""
+class Font(object):
+    """Base class for objects that can return information about a specific typeface."""
+    @property
     def ascent(self):
         """Return the ascent for the given font.
 
@@ -67,6 +24,7 @@ class Metrics(object):
         """
         raise NotImplementedError()
 
+    @property
     def descent(self):
         """Return the descent for the given font.
 
@@ -93,7 +51,27 @@ class Metrics(object):
         """
         raise NotImplementedError()
 
-class ReportlabMetrics(Metrics):
+
+class Library(object):
+    """Base class for objects that can manage information about a collection of fonts."""
+    def __init__(self):
+        pass
+
+    def metrics(style):
+        """Lookup a font using CSS style information and return a corresponding Metrics object.
+
+        Parameters
+        ----------
+        style: dict containing CSS style information
+
+        Returns
+        -------
+        metrics: instance of :class:`toyplot.font.Metrics`
+        """
+        raise NotImplementedError()
+
+
+class ReportlabFont(Font):
     """Use Reportlab to access the metrics for a font.
 
     Parameters
@@ -108,11 +86,13 @@ class ReportlabMetrics(Metrics):
         self._family = family
         self._size = toyplot.units.convert(size, target="pt", default="px")
 
+    @property
     def ascent(self):
         ascent, descent = reportlab.pdfbase.pdfmetrics.getAscentDescent(self._family, self._size) # pylint: disable=unused-variable
         ascent = toyplot.units.convert(ascent, target="px", default="pt")
         return ascent
 
+    @property
     def descent(self):
         ascent, descent = reportlab.pdfbase.pdfmetrics.getAscentDescent(self._family, self._size) # pylint: disable=unused-variable
         descent = toyplot.units.convert(descent, target="px", default="pt")
@@ -122,4 +102,54 @@ class ReportlabMetrics(Metrics):
         width = reportlab.pdfbase.pdfmetrics.stringWidth(string, self._family, self._size)
         width = toyplot.units.convert(width, target="px", default="pt")
         return width
+
+class ReportlabLibrary(Library):
+    """Use Reportlab to provide information about standard PDF fonts."""
+    def family(self, style):
+        """Extract the name of a standard PDF font family from a CSS style dict.
+
+        Parameters
+        ----------
+        style: dict containing CSS key-value pairs.
+        """
+        if "font-family" not in style:
+            raise ValueError("No font family specified: %s" % style)
+
+        bold = True if style.get("font-weight", "") == "bold" else False
+        italic = True if style.get("font-style", "") == "italic" else False
+        for font_family in style["font-family"].split(","):
+            font_family = font_family.lower()
+            if font_family in ReportlabLibrary.family._substitutions:
+                font_family = ReportlabLibrary.family._substitutions[font_family]
+                return ReportlabLibrary.family._font_table[(font_family, bold, italic)]
+
+        raise ValueError("Unknown font family: %s" % style) # pragma: no cover
+
+    family._font_table = {
+        ("courier", False, False): "Courier",
+        ("courier", True, False): "Courier-Bold",
+        ("courier", False, True): "Courier-Oblique",
+        ("courier", True, True): "Courier-BoldOblique",
+        ("helvetica", False, False): "Helvetica",
+        ("helvetica", True, False): "Helvetica-Bold",
+        ("helvetica", False, True): "Helvetica-Oblique",
+        ("helvetica", True, True): "Helvetica-BoldOblique",
+        ("times", False, False): "Times-Roman",
+        ("times", True, False): "Times-Bold",
+        ("times", False, True): "Times-Italic",
+        ("times", True, True): "Times-BoldItalic",
+        }
+
+    family._substitutions = {
+        "courier": "courier",
+        "helvetica": "helvetica",
+        "monospace": "courier",
+        "sans-serif": "helvetica",
+        "serif": "times",
+        "times": "times",
+        }
+
+    def font(self, style):
+        family = self.family(style)
+        return ReportlabFont(family, style["font-size"])
 

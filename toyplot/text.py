cefab4226e165fe948454169cff30f775942efe8
Timothy M. Shead <tshead@sandia.gov>
2017-02-18 17:36:37 -0700

    First iteration with working baselines.

--- toyplot/text.py
+++ toyplot/text.py
@@ -136,14 +136,18 @@ class Newline(Box):
 
 def layout(text, style, fonts):
     def cascade_styles(style, node):
+        if node.tag in ["b", "strong"]:
+            style = toyplot.style.combine(style, {"font-weight": "bold"})
+        elif node.tag in ["code"]:
+            style = toyplot.style.combine(style, {"font-family": "monospace"})
+        elif node.tag in ["em", "i"]:
+            style = toyplot.style.combine(style, {"font-style": "italic"})
+
         if "style" in node.attrib:
             node_style = toyplot.require.style(toyplot.style.parse(node.attrib["style"]), toyplot.require.style.text)
             style = toyplot.style.combine(style, node_style)
-        else:
-            style = copy.deepcopy(style)
 
-        node.style = style
-        #toyplot.log.debug("%s %s %r %r", node.tag, node.style, node.text, node.tail)
+        node.style = copy.deepcopy(style)
         for child in node:
             cascade_styles(style, child)
 
@@ -151,11 +155,24 @@ def layout(text, style, fonts):
         font_size = node.style["font-size"]
         font_size = toyplot.units.convert(font_size, target="px", default="px", reference=reference_font_size)
 
+        baseline_shift = node.style["baseline-shift"]
+        baseline_shift = toyplot.units.convert(baseline_shift, target="px", default="px", reference=reference_font_size)
+
+        if node.tag == "small":
+            font_size *= 0.8
+        elif node.tag == "sub":
+            font_size *= 0.7
+            baseline_shift += 0.2 * font_size
+        elif node.tag == "sup":
+            font_size *= 0.7
+            baseline_shift -= 0.3 * font_size
+
         line_height = node.style["line-height"]
         if line_height == "normal":
             line_height = "120%"
         line_height = toyplot.units.convert(line_height, target="px", default="px", reference=font_size)
 
+        node.style["baseline-shift"] = baseline_shift
         node.style["font-size"] = font_size
         node.style["line-height"] = line_height
 
@@ -164,11 +181,12 @@ def layout(text, style, fonts):
             compute_styles(font_size, child)
 
     def build_formatting_model(node, root=None):
-        toyplot.log.debug("build formatting model: %s", node)
+        #toyplot.log.debug("build formatting model: %s", node)
 
-        if node.tag in ["body"]:
+        if node.tag == "body":
             root = Layout()
 
+        if node.tag in ["body", "b", "code", "i", "em", "small", "span", "strong", "sub", "sup"]:
             if node.text:
                 root.children.append(TextBox(node.text, node.style))
             for child in node:
@@ -177,23 +195,14 @@ def layout(text, style, fonts):
                     root.children.append(TextBox(child.tail, node.style)) # Note: the tail doesn't get the child's style
             return root
 
-        if node.tag in ["b", "code", "i", "em", "small", "span", "strong", "sub", "sup"]:
-            if node.text:
-                root.children.append(TextBox(node.text, node.style))
-            for child in node:
-                build_formatting_model(child, root)
-                if child.tail:
-                    root.children.append(TextBox(child.tail, node.style)) # Note: the tail doesn't get the child's style
-            return
-
         if node.tag == "br":
             root.children.append(Newline())
-            return
+            return root
 
         raise ValueError("Unknown tag: %s" % node.tag)
 
     def create_lines(root):
-        toyplot.log.debug("create lines")
+        #toyplot.log.debug("create lines")
         children = root.children
         root.children = [LineBox()]
         for child in children:
@@ -203,39 +212,61 @@ def layout(text, style, fonts):
                 root.children[-1].children.append(child)
 
     def compute_size(fonts, box):
-        toyplot.log.debug("compute size: %s", box)
+        #toyplot.log.debug("compute size: %s", box)
 
         for child in box.children:
             compute_size(fonts, child)
 
         if isinstance(box, Layout):
-            box.content_width = numpy.max([child.content_width for child in box.children]) if box.children else 0
-            box.content_height = numpy.sum([child.content_height for child in box.children]) if box.children else 0
+            box.width = numpy.max([child.width for child in box.children]) if box.children else 0
+            box.height = numpy.sum([child.height for child in box.children]) if box.children else 0
         elif isinstance(box, LineBox):
-            box.content_width = numpy.sum([child.content_width for child in box.children]) if box.children else 0
-            box.content_height = numpy.max([child.content_height for child in box.children]) if box.children else 0
+            box.width = numpy.sum([child.width for child in box.children]) if box.children else 0
+            box.height = numpy.max([child.height for child in box.children]) if box.children else 0
         elif isinstance(box, TextBox): # This must come before Box
             font = fonts.font(box.style)
-            box.content_width = font.width(box.text)
-            box.content_height = box.style["line-height"]
-            box.text_height = font.ascent - font.descent
+            box.width = font.width(box.text)
+            box.height = box.style["line-height"]
+            box.baseline = (box.height - (font.ascent - font.descent)) * 0.5
         elif isinstance(box, Box):
-            box.content_width = numpy.sum([child.content_width for child in box.children]) if box.children else 0
-            box.content_height = numpy.max([child.content_height for child in box.children]) if box.children else 0
+            box.width = numpy.sum([child.width for child in box.children]) if box.children else 0
+            box.height = numpy.max([child.height for child in box.children]) if box.children else 0
         else:
             raise Exception("Unexpected box type: %s" % box)
 
-    def compute_layout(box, left, top):
-        box.left = left
-        box.top = top
-        box.right = box.left + box.content_width
-        box.bottom = box.top + box.content_height
-        for child in box.children:
-            compute_layout(child, left, top)
-            if isinstance(child, LineBox):
-                top += child.content_height
-            elif isinstance(child, Box):
-                left += child.content_width
+    def compute_position(root):
+        left = 0
+        top = 0
+
+        root.left = left
+        root.top = top
+        root.right = root.left + root.width
+        root.bottom = root.top + root.height
+
+        top = 0
+        for line in root.children:
+            left = 0
+            line.left = left
+            line.top = top
+            line.right = line.left + line.width
+            line.bottom = line.top + line.height
+
+            for child in line.children:
+                child.left = left
+                child.top = top
+                child.right = child.left + child.width
+                child.bottom = child.top + line.height
+                left += child.width
+            top += line.height
+
+    def compute_baseline(fonts, root):
+        for line in root.children:
+            baseline = numpy.min([child.baseline for child in line.children]) if line.children else 0
+            for child in line.children:
+                child.baseline = baseline - child.style["baseline-shift"]
+        for line in root.children:
+            for child in line.children:
+                child.style.pop("baseline-shift", None)
 
     if "font-family" not in style:
         raise ValueError("style must specify font-family")
@@ -245,6 +276,7 @@ def layout(text, style, fonts):
     dom = xml.fromstring(("<body>" + text + "</body>").encode("utf-8"))
 
     default_style = {
+        "baseline-shift": "0",
         "line-height": "normal",
         "vertical-align": "baseline",
     }
@@ -257,7 +289,8 @@ def layout(text, style, fonts):
     root = build_formatting_model(dom)
     create_lines(root)
     compute_size(fonts, root)
-    compute_layout(root, left=0, top=0)
+    compute_position(root)
+    compute_baseline(fonts, root)
 
     return root
 
@@ -273,8 +306,8 @@ def dump(box, stream=None, text=True, style=False, location=False, size=True, le
         stream.write(" %r" % box.style)
     if location:
         stream.write(" %s,%s" % (box.left, box.top))
-    if size and hasattr(box, "content_width") and hasattr(box, "content_height"):
-        stream.write(" %sx%s" % (box.content_width, box.content_height))
+    if size and hasattr(box, "width") and hasattr(box, "height"):
+        stream.write(" %sx%s" % (box.width, box.height))
     stream.write("\n")
     for child in box.children:
         dump(box=child, stream=stream, text=text, style=style, location=location, size=size, level=level+1, indent=indent)

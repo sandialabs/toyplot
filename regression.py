ca406f9f67365e1775977644601cee135baa0307
Timothy M. Shead <tshead@sandia.gov>
2017-04-02 20:58:00 -0600

    Turn-off showing XML diffs for failing tests by default.

--- regression.py
+++ regression.py
@@ -7,6 +7,6 @@ import subprocess
 subprocess.call(["coverage", "run", "--source", "toyplot",
                  "--omit", "toyplot/testing.py", "-m", "nose", "--exclude-dir", "toyplot"])
 subprocess.call(["coverage", "run", "--append", "--source",
-                 "toyplot", "--omit", "toyplot/testing.py", "-m", "behave"])
+                 "toyplot", "--omit", "toyplot/testing.py", "-m", "behave", "-D", "show_diff=False"])
 subprocess.call(["coverage", "report"])
 subprocess.call(["coverage", "html", "--directory", ".cover"])

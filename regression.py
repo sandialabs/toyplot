64386b804dd4a147ee755ac82dd60b91721ad8b7
Timothy M. Shead <tshead@sandia.gov>
2017-05-05 14:30:22 -0600

    Make it easier to enable diffs for failed tests.

--- regression.py
+++ regression.py
@@ -2,11 +2,16 @@
 # DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
 # rights in this software.
 
+import argparse
 import subprocess
+
+parser = argparse.ArgumentParser("Run all Toyplot regression tests.")
+parser.add_argument("--show-diff", action="store_true", help="Display differences between failed tests and references.")
+arguments = parser.parse_args()
 
 subprocess.call(["coverage", "run", "--source", "toyplot",
                  "--omit", "toyplot/testing.py", "-m", "nose", "--exclude-dir", "toyplot"])
 subprocess.call(["coverage", "run", "--append", "--source",
-                 "toyplot", "--omit", "toyplot/testing.py", "-m", "behave", "-D", "show_diff=False"])
+                 "toyplot", "--omit", "toyplot/testing.py", "-m", "behave", "-D", "show_diff=True" if arguments.show_diff else "show_diff=False"])
 subprocess.call(["coverage", "report"])
 subprocess.call(["coverage", "html", "--directory", ".cover"])

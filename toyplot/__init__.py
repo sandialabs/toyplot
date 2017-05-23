9bb9fb201baf4ab468e82d87feb3fee03d35d9cd
Timothy M. Shead <tshead@sandia.gov>
2017-05-23 15:00:26 -0600

    Add margin parameters to the convenience API and sort keyword parameters alphabetically for complex function signatures.

--- toyplot/__init__.py
+++ toyplot/__init__.py
@@ -26,25 +26,26 @@ def bars(
         along="x",
         baseline="stacked",
         color=None,
-        opacity=1.0,
-        title=None,
-        style=None,
         filename=None,
-        xmin=None,
-        xmax=None,
-        ymin=None,
-        ymax=None,
-        show=True,
-        xshow=True,
-        yshow=True,
-        label=None,
-        xlabel=None,
-        ylabel=None,
-        xscale="linear",
-        yscale="linear",
-        padding=10,
-        width=None,
         height=None,
+        label=None,
+        margin=50,
+        opacity=1.0,
+        padding=10,
+        show=True,
+        style=None,
+        title=None,
+        width=None,
+        xlabel=None,
+        xmax=None,
+        xmin=None,
+        xscale="linear",
+        xshow=True,
+        ylabel=None,
+        ymax=None,
+        ymin=None,
+        yscale="linear",
+        yshow=True,
     ):
     """Convenience function for creating a bar plot in a single call.
 
@@ -63,19 +64,22 @@ def bars(
     """
     canvas = Canvas(width=width, height=height)
     axes = canvas.cartesian(
-        xmin=xmin,
-        xmax=xmax,
-        ymin=ymin,
-        ymax=ymax,
-        show=show,
-        xshow=xshow,
-        yshow=yshow,
         label=label,
+        margin=margin,
+        padding=padding,
+        show=show,
         xlabel=xlabel,
-        ylabel=ylabel,
+        xmax=xmax,
+        xmin=xmin,
         xscale=xscale,
+        xshow=xshow,
+        ylabel=ylabel,
+        ymax=ymax,
+        ymin=ymin,
         yscale=yscale,
-        padding=padding)
+        yshow=yshow,
+        )
+
     mark = axes.bars(
         a=a,
         b=b,
@@ -83,10 +87,11 @@ def bars(
         along=along,
         baseline=baseline,
         color=color,
+        filename=filename,
         opacity=opacity,
-        title=title,
         style=style,
-        filename=filename)
+        title=title,
+        )
     return canvas, axes, mark
 
 
@@ -97,25 +102,26 @@ def fill(
         along="x",
         baseline=None,
         color=None,
-        opacity=1.0,
-        title=None,
-        style=None,
         filename=None,
-        xmin=None,
-        xmax=None,
-        ymin=None,
-        ymax=None,
-        show=True,
-        xshow=True,
-        yshow=True,
-        label=None,
-        xlabel=None,
-        ylabel=None,
-        xscale="linear",
-        yscale="linear",
-        padding=10,
-        width=None,
         height=None,
+        label=None,
+        margin=50,
+        opacity=1.0,
+        padding=10,
+        show=True,
+        style=None,
+        title=None,
+        width=None,
+        xlabel=None,
+        xmax=None,
+        xmin=None,
+        xscale="linear",
+        xshow=True,
+        ylabel=None,
+        ymax=None,
+        ymin=None,
+        yscale="linear",
+        yshow=True,
     ):
     """Convenience function for creating a fill plot in a single call.
 
@@ -132,21 +138,26 @@ def fill(
     mark: :class:`toyplot.mark.FillBoundaries` or :class:`toyplot.mark.FillMagnitudes`
       The new bar mark.
     """
-    canvas = Canvas(width=width, height=height)
+    canvas = Canvas(
+        height=height,
+        width=width,
+        )
     axes = canvas.cartesian(
-        xmin=xmin,
-        xmax=xmax,
-        ymin=ymin,
-        ymax=ymax,
-        show=show,
-        xshow=xshow,
-        yshow=yshow,
         label=label,
+        margin=margin,
+        padding=padding,
+        show=show,
         xlabel=xlabel,
-        ylabel=ylabel,
+        xmax=xmax,
+        xmin=xmin,
         xscale=xscale,
+        xshow=xshow,
+        ylabel=ylabel,
+        ymax=ymax,
+        ymin=ymin,
         yscale=yscale,
-        padding=padding)
+        yshow=yshow,
+        )
     mark = axes.fill(
         a=a,
         b=b,
@@ -154,10 +165,11 @@ def fill(
         along=along,
         baseline=baseline,
         color=color,
+        filename=filename,
         opacity=opacity,
-        title=title,
         style=style,
-        filename=filename)
+        title=title,
+        )
     return canvas, axes, mark
 
 
@@ -165,27 +177,28 @@ def graph(
         a,
         b=None,
         c=None,
-        olayout=None,
-        layout=None,
         along="x",
-        vlabel=None,
-        vcoordinates=None,
-        vcolor=None,
-        vmarker="o",
-        varea=None,
-        vsize=None,
-        vopacity=1.0,
-        vtitle=None,
-        vstyle=None,
-        vlstyle=None,
-        vlshow=True,
         ecolor=None,
-        ewidth=1.0,
         eopacity=1.0,
         estyle=None,
-        padding=20,
-        width=None,
+        ewidth=1.0,
         height=None,
+        layout=None,
+        margin=50,
+        olayout=None,
+        padding=20,
+        varea=None,
+        vcolor=None,
+        vcoordinates=None,
+        vlabel=None,
+        vlshow=True,
+        vlstyle=None,
+        vmarker="o",
+        vopacity=1.0,
+        vsize=None,
+        vstyle=None,
+        vtitle=None,
+        width=None,
     ): # pragma: no cover
     """Convenience function for creating a graph plot in a single call.
 
@@ -202,38 +215,47 @@ def graph(
     mark: :class:`toyplot.mark.Graph`
         The new graph mark.
     """
-    canvas = Canvas(width=width, height=height)
-    axes = canvas.cartesian(aspect="fit-range", show=False, padding=padding)
+    canvas = Canvas(
+        height=height,
+        width=width,
+        )
+    axes = canvas.cartesian(
+        aspect="fit-range",
+        margin=margin,
+        padding=padding,
+        show=False,
+        )
     mark = axes.graph(
         a=a,
         b=b,
         c=c,
-        olayout=olayout,
-        layout=layout,
         along=along,
-        vlabel=vlabel,
-        vcoordinates=vcoordinates,
-        vcolor=vcolor,
-        vmarker=vmarker,
-        varea=varea,
-        vsize=vsize,
-        vopacity=vopacity,
-        vtitle=vtitle,
-        vstyle=vstyle,
-        vlstyle=vlstyle,
-        vlshow=vlshow,
         ecolor=ecolor,
-        ewidth=ewidth,
         eopacity=eopacity,
         estyle=estyle,
+        ewidth=ewidth,
+        layout=layout,
+        olayout=olayout,
+        varea=varea,
+        vcolor=vcolor,
+        vcoordinates=vcoordinates,
+        vlabel=vlabel,
+        vlshow=vlshow,
+        vlstyle=vlstyle,
+        vmarker=vmarker,
+        vopacity=vopacity,
+        vsize=vsize,
+        vstyle=vstyle,
+        vtitle=vtitle,
         )
     return canvas, axes, mark
 
 
 def image(
         data,
-        width=None,
         height=None,
+        margin=0,
+        width=None,
     ): # pragma: no cover
     """Convenience function for displaying an image in a single call.
 
@@ -247,34 +269,38 @@ def image(
     mark: :class:`toyplot.mark.Image`
         The new image mark.
     """
-    canvas = Canvas(width=width, height=height)
+    canvas = Canvas(
+        height=height,
+        width=width,
+        )
     mark = canvas.image(
         data=data,
-        margin=0,
+        margin=margin,
         )
     return canvas, mark
 
 
 def matrix(
         data,
-        label=None,
-        tlabel=None,
-        llabel=None,
-        rlabel=None,
         blabel=None,
-        step=1,
-        tshow=None,
-        lshow=None,
-        rshow=None,
-        bshow=None,
-        tlocator=None,
-        llocator=None,
-        rlocator=None,
         blocator=None,
+        bshow=None,
         colorshow=False,
-        width=None,
-        height=None,
         filename=None,
+        height=None,
+        label=None,
+        llabel=None,
+        llocator=None,
+        lshow=None,
+        margin=50,
+        rlabel=None,
+        rlocator=None,
+        rshow=None,
+        step=1,
+        tlabel=None,
+        tlocator=None,
+        tshow=None,
+        width=None,
     ):
     """Convenience function to create a matrix visualization in a single call.
 
@@ -288,25 +314,29 @@ def matrix(
     table: :class:`toyplot.coordinates.Table`
       A new set of table axes that fill the canvas.
     """
-    canvas = Canvas(width=width, height=height)
+    canvas = Canvas(
+        height=height,
+        width=width,
+        )
     axes = canvas.matrix(
-        data=data,
-        label=label,
-        tlabel=tlabel,
-        llabel=llabel,
-        rlabel=rlabel,
         blabel=blabel,
-        step=step,
-        tshow=tshow,
-        lshow=lshow,
-        rshow=rshow,
-        bshow=bshow,
-        tlocator=tlocator,
-        llocator=llocator,
-        rlocator=rlocator,
         blocator=blocator,
+        bshow=bshow,
         colorshow=colorshow,
+        data=data,
         filename=filename,
+        label=label,
+        llabel=llabel,
+        llocator=llocator,
+        lshow=lshow,
+        margin=margin,
+        rlabel=rlabel,
+        rlocator=rlocator,
+        rshow=rshow,
+        step=step,
+        tlabel=tlabel,
+        tlocator=tlocator,
+        tshow=tshow,
         )
     return canvas, axes
 
@@ -315,36 +345,37 @@ def plot(
         a,
         b=None,
         along="x",
-        color=None,
-        stroke_width=2.0,
-        opacity=1.0,
-        title=None,
-        marker=None,
         area=None,
-        size=None,
-        mfill=None,
-        mopacity=1.0,
-        mtitle=None,
-        style=None,
-        mstyle=None,
-        mlstyle=None,
-        filename=None,
         aspect=None,
-        xmin=None,
-        xmax=None,
-        ymin=None,
-        ymax=None,
-        show=True,
-        xshow=True,
-        yshow=True,
-        label=None,
-        xlabel=None,
-        ylabel=None,
-        xscale="linear",
-        yscale="linear",
-        padding=10,
-        width=None,
+        color=None,
+        filename=None,
         height=None,
+        label=None,
+        margin=50,
+        marker=None,
+        mfill=None,
+        mlstyle=None,
+        mopacity=1.0,
+        mstyle=None,
+        mtitle=None,
+        opacity=1.0,
+        padding=10,
+        show=True,
+        size=None,
+        stroke_width=2.0,
+        style=None,
+        title=None,
+        width=None,
+        xlabel=None,
+        xmax=None,
+        xmin=None,
+        xscale="linear",
+        xshow=True,
+        ylabel=None,
+        ymax=None,
+        ymin=None,
+        yscale="linear",
+        yshow=True,
     ):
     """Convenience function for creating a line plot in a single call.
 
@@ -361,40 +392,46 @@ def plot(
     mark: :class:`toyplot.mark.Plot`
       The new plot mark.
     """
-    canvas = Canvas(width=width, height=height)
+    canvas = Canvas(
+        height=height,
+        width=width,
+        )
     axes = canvas.cartesian(
         aspect=aspect,
-        xmin=xmin,
-        xmax=xmax,
-        ymin=ymin,
-        ymax=ymax,
-        show=show,
-        xshow=xshow,
-        yshow=yshow,
         label=label,
+        margin=margin,
+        padding=padding,
+        show=show,
         xlabel=xlabel,
-        ylabel=ylabel,
+        xmax=xmax,
+        xmin=xmin,
         xscale=xscale,
+        xshow=xshow,
+        ylabel=ylabel,
+        ymax=ymax,
+        ymin=ymin,
         yscale=yscale,
-        padding=padding)
+        yshow=yshow,
+        )
     mark = axes.plot(
         a=a,
         b=b,
         along=along,
-        color=color,
-        stroke_width=stroke_width,
-        opacity=opacity,
-        title=title,
-        marker=marker,
         area=area,
-        size=size,
+        color=color,
+        filename=filename,
+        marker=marker,
         mfill=mfill,
-        mopacity=mopacity,
-        mtitle=mtitle,
-        style=style,
-        mstyle=mstyle,
         mlstyle=mlstyle,
-        filename=filename)
+        mopacity=mopacity,
+        mstyle=mstyle,
+        mtitle=mtitle,
+        opacity=opacity,
+        size=size,
+        stroke_width=stroke_width,
+        style=style,
+        title=title,
+        )
     return canvas, axes, mark
 
 
@@ -402,32 +439,33 @@ def scatterplot(
         a,
         b=None,
         along="x",
-        color=None,
-        marker="o",
         area=None,
-        size=None,
-        opacity=1.0,
-        title=None,
-        style=None,
-        mstyle=None,
-        mlstyle=None,
-        filename=None,
         aspect=None,
-        xmin=None,
-        xmax=None,
-        ymin=None,
-        ymax=None,
-        show=True,
-        xshow=True,
-        yshow=True,
-        label=None,
-        xlabel=None,
-        ylabel=None,
-        xscale="linear",
-        yscale="linear",
-        padding=10,
-        width=None,
+        color=None,
+        filename=None,
         height=None,
+        label=None,
+        margin=50,
+        marker="o",
+        mlstyle=None,
+        mstyle=None,
+        opacity=1.0,
+        padding=10,
+        show=True,
+        size=None,
+        style=None,
+        title=None,
+        width=None,
+        xlabel=None,
+        xmax=None,
+        xmin=None,
+        xscale="linear",
+        xshow=True,
+        ylabel=None,
+        ymax=None,
+        ymin=None,
+        yscale="linear",
+        yshow=True,
     ):
     """Convenience function for creating a scatter plot in a single call.
 
@@ -444,51 +482,58 @@ def scatterplot(
     mark: :class:`toyplot.mark.Plot`
       The new scatter plot mark.
     """
-    canvas = Canvas(width=width, height=height)
+    canvas = Canvas(
+        height=height,
+        width=width,
+        )
     axes = canvas.cartesian(
         aspect=aspect,
-        xmin=xmin,
-        xmax=xmax,
-        ymin=ymin,
-        ymax=ymax,
-        show=show,
-        xshow=xshow,
-        yshow=yshow,
         label=label,
+        margin=margin,
+        padding=padding,
+        show=show,
         xlabel=xlabel,
-        ylabel=ylabel,
+        xmax=xmax,
+        xmin=xmin,
         xscale=xscale,
+        xshow=xshow,
+        ylabel=ylabel,
+        ymax=ymax,
+        ymin=ymin,
         yscale=yscale,
-        padding=padding)
+        yshow=yshow,
+        )
     mark = axes.scatterplot(
         a=a,
         b=b,
         along=along,
-        color=color,
-        marker=marker,
         area=area,
-        size=size,
-        opacity=opacity,
-        title=title,
-        style=style,
-        mstyle=mstyle,
+        color=color,
+        filename=filename,
+        marker=marker,
         mlstyle=mlstyle,
-        filename=filename)
+        mstyle=mstyle,
+        opacity=opacity,
+        size=size,
+        style=style,
+        title=title,
+        )
     return canvas, axes, mark
 
 
 def table(
         data=None,
-        rows=None,
-        columns=None,
-        trows=None,
         brows=None,
-        lcolumns=None,
-        rcolumns=None,
-        label=None,
-        width=None,
-        height=None,
+        columns=None,
         filename=None,
+        height=None,
+        label=None,
+        lcolumns=None,
+        margin=50,
+        rcolumns=None,
+        rows=None,
+        trows=None,
+        width=None,
     ):
     """Convenience function to create a table visualization in a single call.
 
@@ -502,16 +547,21 @@ def table(
     table: :class:`toyplot.coordinates.Table`
       A new set of table axes that fill the canvas.
     """
-    canvas = Canvas(width=width, height=height)
+    canvas = Canvas(
+        height=height,
+        width=width,
+        )
     axes = canvas.table(
-        data=data,
-        rows=rows,
-        columns=columns,
-        trows=trows,
         brows=brows,
-        lcolumns=lcolumns,
-        rcolumns=rcolumns,
-        label=label,
+        columns=columns,
+        data=data,
         filename=filename,
+        label=label,
+        lcolumns=lcolumns,
+        margin=margin,
+        rcolumns=rcolumns,
+        rows=rows,
+        trows=trows,
         )
     return canvas, axes
+

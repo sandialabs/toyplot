74654fbdbd96d60af1fdb50fe0a765a83627649c
Timothy M. Shead <tshead@sandia.gov>
2017-09-01 10:11:15 -0600

    Update toyplot.compatibility docstring.

--- toyplot/compatibility.py
+++ toyplot/compatibility.py
@@ -2,9 +2,12 @@
 # DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
 # rights in this software.
 
-"""Toyplot has been implemented to work equally well in Python 2 and Python 3,
-without the use of code-modification tools like `2to3`.  The
-`toyplot.compatibility` module contains code to facilitate this.
+"""Python 2/3 compatibility code.
+
+Toyplot is designed to work equally well in Python 2 and Python 3 without the
+need for code-modification tools like `2to3` or `six`.  This module is
+simplifies writing portable code, and should not be needed by Toyplot
+end-users.
 """
 
 from __future__ import division

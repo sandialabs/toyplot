37e8f74d5e0df76cd74caa427e7a08bae400a99c
Dana Huget <danaliyahuget@gmail.com>
2018-04-03 21:16:34 -0700

    added implementation unit/currency regression tests

--- features/steps/format.py
+++ features/steps/format.py
@@ -54,3 +54,68 @@ def step_impl(context):
     nose.tools.assert_equal(prefix, "1")
     nose.tools.assert_equal(separator, "")
     nose.tools.assert_equal(suffix, "")
+ 
+
+@given(u'an instance of toyplot.format.UnitFormatter')
+def step_impl(context):
+    context.formatter = toyplot.format.UnitFormatter()
+
+
+@then(
+    u'formatting inch units with the toyplot.format.UnitFormatter should produce valid output')
+def step_impl(context):
+    val = context.formatter.format(12.2, "inches")
+    prefix, separator, suffix, units = val
+    nose.tools.assert_equal(prefix, "12")
+    nose.tools.assert_equal(separator, ".")
+    nose.tools.assert_equal(suffix, "2")
+    nose.tools.assert_equal(units, "in")
+
+
+@then(
+    u'formatting point units with the toyplot.format.UnitFormatter should produce valid output')
+def step_impl(context):
+    val = context.formatter.format(5.1, "points")
+    prefix, separator, suffix, units = val
+    nose.tools.assert_equal(prefix, "5")
+    nose.tools.assert_equal(separator, ".")
+    nose.tools.assert_equal(suffix, "1")
+    nose.tools.assert_equal(units, "pt")
+
+
+@given(u'an instance of toyplot.format.CurrencyFormatter')
+def step_impl(context):
+    context.formatter = toyplot.format.CurrencyFormatter(curr="cad")
+
+
+@then(
+    u'formatting Canadian currency with the toyplot.format.CurrencyFormatter should produce valid output')
+def step_impl(context):
+    codes, prefix, dp, suffix = context.formatter.format(100.00)
+    nose.tools.assert_equal(codes, "$")
+    nose.tools.assert_equal(prefix, "100")
+    nose.tools.assert_equal(dp, ".")
+    nose.tools.assert_equal(suffix, "00")
+
+
+@then(
+    u'formatting European currency with the toyplot.format.CurrencyFormatter should produce valid output')
+def step_impl(context):
+    context.formatter = toyplot.format.CurrencyFormatter(curr="eur")
+    val = context.formatter.format(9000.56)
+    codes, prefix, dp, suffix = val
+    nose.tools.assert_equal(codes, "€")
+    nose.tools.assert_equal(prefix, "9,000")
+    nose.tools.assert_equal(dp, ".")
+    nose.tools.assert_equal(suffix, "56")
+
+@then(
+    u'formatting British currency with the toyplot.format.CurrencyFormatter should produce valid output')
+def step_impl(context):
+    context.formatter = toyplot.format.CurrencyFormatter(curr="gbp")
+    val = context.formatter.format(23423410.5)
+    codes, prefix, dp, suffix = val
+    nose.tools.assert_equal(codes, "£")
+    nose.tools.assert_equal(prefix, "23,423,410")
+    nose.tools.assert_equal(dp, ".")
+    nose.tools.assert_equal(suffix, "50")

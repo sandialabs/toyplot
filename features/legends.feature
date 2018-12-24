169fb4990b28a6bf0edae1d6ee04788638edc7de
Timothy M. Shead <tshead@sandia.gov>
2018-12-23 17:31:20 -0700

    Organize legend regression tests.

--- features/legends.feature
+++ features/legends.feature
@@ -2,16 +2,16 @@ Feature: Legends
   Scenario Outline: Render legends
     Given a default canvas
     And a set of cartesian axes
-    And <data>
-    And the data is rendered as <mark>
-    And the marks are added to a legend
-    And the legend is added to the <target>
+    And <scenario>
+    And the marks are used to create a legend
     Then the figure should match the <reference> reference image
 
     Examples:
-      | data                                    | mark       | target    | reference                       |
-      | single and multi series data            | bars       | canvas    | legend-canvas-bar               |
-      | single and multi series data            | fills       | canvas    | legend-canvas-fill               |
-      | single and multi series data            | plots       | canvas    | legend-canvas-plot               |
-      | single and multi series data            | scatterplots       | canvas    | legend-canvas-scatterplot               |
+      | scenario                                | reference                       |
+      | the data is rendered as boundary bars   | legend-boundary-bars               |
+      | the data is rendered as magnitude bars  | legend-magnitude-bars               |
+      | the data is rendered as boundary fills  | legend-boundary-fills               |
+      | the data is rendered as magnitude fills  | legend-magnitude-fills               |
+      | the data is rendered as plots           | legend-plots               |
+      | the data is rendered as scatterplots    | legend-scatterplots               |
 
--- features/steps/legends.py
+++ features/steps/legends.py
@@ -6,35 +6,47 @@ from behave import *
 import nose.tools
 import numpy
 
-@given(u'single and multi series data')
-def step_impl(context):
-    context.data = []
-    context.data.append(numpy.column_stack((numpy.linspace(0, 1), numpy.linspace(0, 1) ** 2)))
-    context.data.append(numpy.linspace(0, 1) ** 3)
+def sample_data():
+    data = []
+    data.append(numpy.column_stack((numpy.linspace(0, 1), numpy.linspace(0, 1) ** 2)))
+    data.append(numpy.linspace(0, 1) ** 3)
+    return data
 
-@given(u'the data is rendered as bars')
-def step_impl(context):
-    context.marks = [context.axes.bars(data) for data in context.data]
 
-@given(u'the data is rendered as fills')
+@given(u'the data is rendered as boundary bars')
 def step_impl(context):
-    context.marks = [context.axes.fill(data, baseline="symmetric") for data in context.data]
+    context.marks = [context.axes.bars(data, baseline=None) for data in sample_data()]
+
+
+@given(u'the data is rendered as boundary fills')
+def step_impl(context):
+    context.marks = [context.axes.fill(data, baseline=None) for data in sample_data()]
+
+
+@given(u'the data is rendered as magnitude bars')
+def step_impl(context):
+    context.marks = [context.axes.bars(data, baseline="symmetric") for data in sample_data()]
+
+
+@given(u'the data is rendered as magnitude fills')
+def step_impl(context):
+    context.marks = [context.axes.fill(data, baseline="symmetric") for data in sample_data()]
+
 
 @given(u'the data is rendered as plots')
 def step_impl(context):
-    context.marks = [context.axes.plot(data) for data in context.data]
+    context.marks = [context.axes.plot(data) for data in sample_data()]
+
 
 @given(u'the data is rendered as scatterplots')
 def step_impl(context):
-    context.marks = [context.axes.scatterplot(data) for data in context.data]
+    context.marks = [context.axes.scatterplot(data) for data in sample_data()]
 
-@given(u'the marks are added to a legend')
-def step_impl(context):
-    context.entries = [("Mark %s" % index, mark) for index, mark in enumerate(context.marks)]
 
-@given(u'the legend is added to the canvas')
+@given(u'the marks are used to create a legend')
 def step_impl(context):
-    table = context.canvas.legend(context.entries, corner=("top-left", 50, 100, 25 * len(context.entries)))
+    entries = [("Mark %s" % index, mark) for index, mark in enumerate(context.marks)]
+    table = context.canvas.legend(entries, corner=("top-left", 50, 100, 25 * len(entries)))
     table.cells.grid.hlines[...] = "single"
     table.cells.grid.vlines[...] = "single"
 

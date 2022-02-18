e9c7de9a5df5bef4b75e6df1f38e45c366288e8a
Timothy M. Shead <tshead@sandia.gov>
2022-02-18 11:49:10 -0700

    Bump version number.

--- toyplot/__init__.py
+++ toyplot/__init__.py
@@ -14,7 +14,7 @@ if sys.version_info.major < 3:
 
 from toyplot.canvas import Canvas
 
-__version__ = "1.0.1"
+__version__ = "1.0.2-dev"
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.WARNING)

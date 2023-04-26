e514193337253469d275f40aef58781c2e413b5f
Timothy M. Shead <tshead@sandia.gov>
2023-04-26 15:11:24 -0600

    Update documentation conf.

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = "Toyplot"
 copyright = u"""2014, Sandia Corporation. Under the terms of Contract
 DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
 rights in this software"""
-author = 'Timothy M. Shead'
+author = "Timothy M. Shead"
 
 # The full version, including alpha/beta/rc tags
 import toyplot
@@ -48,8 +48,6 @@ extensions = [
     "sphinx_rtd_theme",
 ]
 
-#napoleon_use_param = False
-
 intersphinx_mapping = {
     "arrow": ("http://arrow.readthedocs.io/en/latest", "arrow.inv"),
     "numpy": ("http://docs.scipy.org/doc/numpy-1.13.0", "numpy.inv"),
@@ -58,8 +56,10 @@ intersphinx_mapping = {
     "PIL": ("http://pillow.readthedocs.io/en/3.2.x", "pillow.inv"),
     }
 
+tls_verify = False
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The master toctree document.
 master_doc = "index"
@@ -67,12 +67,12 @@ master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- nbsphinx options --------------------------------------------------------
 
-nbsphinx_execute = "never" if "READTHEDOCS" in os.environ else "always"
+nbsphinx_execute = "never"
 
 nbsphinx_prolog = """
  .. raw:: html
@@ -90,13 +90,13 @@ nbsphinx_prolog = """
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+#html_static_path = ["_static"]
 
 def warn_undocumented_members(app, what, name, obj, options, lines):
     if what not in [] and len(lines) == 0:
